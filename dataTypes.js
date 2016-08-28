17dfaf5968ba00bc8db08f675fb1a32697021ffc
Vince <vince@riversconsulting.com>
2016-08-27 23:10:13 -0400

    Update dataTypes.js

--- dataTypes.js
+++ dataTypes.js
@@ -1,7 +1,40 @@
+/*
+
 Null:
 
-  You have defined a variable and you know what Data Type it is, but have not given it a value.
+  You have defined a variable and you know what Data Type it is, but have not given it a value. It is a placeholder for a variable's value.  A variable that has no value yet.
 
 Undefined:
 
-  You have no idea what the data Type is for this value.
+  You have no idea what the Data Type is for this variable yet.  The variable has been declared, but it is an unused variable.
+
+String:
+
+  text, always wrapped with quotes.
+
+Numbers:
+
+  Are numbers with all the same characteristics of numbers in the real world.  (You can perform operations on them).
+
+Boolean:
+
+  A value of True or False.  ....takes the least amount of memory. (1 bit  --> 0 or 1).
+
+Function:
+
+  Chunk of reusable code.
+
+Object:
+
+
+
+
+
+
+
+
+=================================
+What is a higher order language?   ....You have a lot of flexibility with the language.  You can pass functions as variables.  
+(JavaScript is a high order language)
+=================================
+*/
