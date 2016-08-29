5c0b9da8755c19cf7a06e0e2442ecd81eaadac4c
Vince <vince@riversconsulting.com>
2016-08-29 17:17:49 -0400

    Update objects.js

--- objects.js
+++ objects.js
@@ -62,5 +62,43 @@ removing a property from an object:
               delete emptyObj.key2;                           <---- removing a property from an object
               console.log(emptyObj);                          <---- { key1: "value1" }
 
+for/in loop:
+
+              var myObj = {};
+              myObj = {names: "Vin", gender: "male", graduate: true};
+              for (var k in myObj){
+                  console.log(k);
+                  console.log(myObj[k] + "\n");                        <---- interpret k so must use bracket notation
+              }
+
+              results:
+
+                        names
+                        Vin
+
+                        gender
+                        male
+
+                        graduate
+                        true
+
+              The "for / in" loop loops through the entire object.  The data that we are looking at are pieces of a record, namely
+              property/value (or key/value) pairs of one record which is the object itself.
+              
+              A DBMS would be a collection of objects kind of like this:
+              
+              var person1 = { name: "Vin", gender: "M"};              <---- objects
+              var person2 = { name: "Karen", gender: "F"};
+              var person3 = {name: "Ryan". gender: "M"};
+              var people = [person1, person2, person3];               <---- this array would be more of a database           
+
+
+
+
+
+
+
+
+
 
 
