e0d7e4769dd9cf6f5c7c66615f7784572f430d01
Vince <vince@riversconsulting.com>
2016-08-29 14:54:57 -0400

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,5 +1,18 @@
 /* Objects
 
-Look at objects as a bag of keys ("strings") and when you pull a key (or know a particular key)(use a key), you can access 
-that key's values. So we have a bunch of doors and these keys open the door to the values inside.
+Look at objects as a bag of keys ("strings") and when you pull out a key (or know a particular key)(use a key), you can access 
+that key's values. So we have a bunch of doors and these keys open the door to the values inside.  Unless we have that key, we
+can't access the value. ......Key : Value.    .....key (===property) : Value.
+
+As an example:  If you go to a dictionary (an object is similar to a dictionary).  You would go to a word in the dictionary 
+(The Key or the property) and then you can see the definition (Value for the word).
+
+              var dictionary = {                        <----  the braces tell JS that this variable is an object and the 
+                                                              contents (property: value pairs) are within the curly braces.
+                  sunny: "cheerful, optimistic",        <----  property: "value" or "value1, value2..."
+                  design: "contrive, devise"
+              };
+
+
+
 
