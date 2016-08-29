ab162dfd4ed6f9807d9f47ddca92fe8d520ee18d
Vince <vince@riversconsulting.com>
2016-08-29 16:30:12 -0400

    Update objects.js

--- objects.js
+++ objects.js
@@ -7,21 +7,60 @@ can't access the value. ......Key : Value.    .....key (===property) : Value.
 As an example:  If you go to a dictionary (an object is similar to a dictionary).  You would go to a word in the dictionary 
 (The Key or the property) and then you can see the definition (Value for the word).
 
-              var dictionary = {                        <----  the braces tell JS that this variable is an object and the 
-                  sunny: "cheerful, optimistic",                contents (property: value pairs) are within the curly braces.
-                  design: "contrive, devise"            <----  property: "value" or "value1, value2..."
-              };                                        <----  Note:  The value in this case is a string, but it can be
-                                                                whatever datatype you want it to be.  The key(property)
-                                                                must always be a string. Written either:  name: or "name":
-                                                                
+Defining an Object:
+
+              var dictionary = {                          <----  the braces tell JS that this variable is an object and the 
+                    sunny: "cheerful, optimistic",                contents (property: value pairs) are within the curly braces.
+                    design: "contrive, devise"            <----  property: "value" or "value1, value2..."
+              };                                          <----  Note:  The value in this case is a string, but it can be
+                                                                  whatever datatype you want it to be.  The key(property)
+                                                                  must always be a string. Written either:  name: or "name":
+
               var person1 = {
-                  name: "Alex",
-                  age: 31,
-                  degree: true,
-                  eyes: "Brown"
-              };
+                    name: "Alex",
+                    age: 31,
+                    degree: true,
+                    eyes: "Brown"
 
+              var unusualObj = {
+                    array: [1,2,3,[4,5]],                     <---- an array with another nested array inside it
+                    resultBoolean: true,                      <---- a boolean value
+                    str: "this is a string",                  <---- a string value
+                    nestedObj: {                              <---- a nested Object
+                      str2: "a different string",
+                      anotherObj: {                           <---- another more deeply nested object
+                        key: "string value",
+                      }
+                    }
+                  }
 
+Adding/Referencing properties:
+
+              var emptyObj = {};                              <---- initialize variable as an empty object
+              
+              emptyObj.key1 = "value1";                       <---- assign 1 key: value pair
+              emptyObj["key2"] = "value2";                    <---- assign another value pair using []
+              
+              console.log(emptyObj);                          <---- { key1: "value1", key2: "value2" }
+              
+              console.log(emptyObj.key1);                     <---- "value1"
+              console.log(emptyObj["key1"]);                  <---- "value1"
+              console.log(emptyObj.key2);                     <---- "value2"
+              console.log(emptyObj["key2"]);                  <---- "value2"
+              console.log(emptyObj.key3);                     <---- undefined (since you never entered anything for a key3 property)
+              
+              var strKey = "key2";
+              console.log(emptyObj[strKey]);                  <---- "value2"  
+                                                                      remember that Keys (Properties) must always be a string.
+                                                                      so here is when you must use bracket notation to 
+                                                                      identify values for keys that are assigned to this 
+                                                                      variable called strKey.  ...use bracket notation when
+                                                                      you are uncertain what you are looking up. 
+
+removing a property from an object:
+
+              delete emptyObj.key2;                           <---- removing a property from an object
+              console.log(emptyObj);                          <---- { key1: "value1" }
 
 
 
