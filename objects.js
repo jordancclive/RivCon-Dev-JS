0628a6cea9980184a185696731711aeb653afcf7
Vince <vince@riversconsulting.com>
2016-08-29 14:36:33 -0400

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 /* Objects
 
-Look at objects as a bag of keys ("strings") and when you pull (or know a particular key)(use a key), you can access that key's values.  
-So we have a bunch of doors and these keys open the door to the values inside.
+Look at objects as a bag of keys ("strings") and when you pull a key (or know a particular key)(use a key), you can access 
+that key's values. So we have a bunch of doors and these keys open the door to the values inside.
 
