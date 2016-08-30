5672d4db4a41540f190cd2079fdf7b5282f5cc54
Vince <vince@riversconsulting.com>
2016-08-30 14:58:28 -0400

    Create truthy-Falsey.js

---
+++ truthy-Falsey.js
@@ -0,0 +1,26 @@
+// Truthy-Falsey
+// Every statement in javaScript can be evaluated as true or false.     (called Truthy & Falsey)
+/*
+
+A Truthy statement is a value that translates to true when evaluated in a Boolean context.
+        By default values evaluat as truth.
+
+A Falsey value is a value that translates to false when evaluated in a Boolean context.
+        Falsey items:
+        - false
+        - 0
+        
+        
+        
+        
+*/
+// An illustration:
+
+// You might normally do this:
+
+                function countdown(n){
+                    while(n>0){
+                    
+                    
+                }
+
