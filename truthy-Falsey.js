c778f218e8a2a6c465abb9480b7d685f98d8013c
Vince <vince@riversconsulting.com>
2016-08-30 15:15:23 -0400

    Update truthy-Falsey.js

--- truthy-Falsey.js
+++ truthy-Falsey.js
@@ -9,9 +9,24 @@ A Falsey value is a value that translates to false when evaluated in a Boolean c
         Falsey items:
         - false
         - 0
-        
-        
-        
+        - ""   <---- an empty string
+        - null
+        - undefined
+        - NaN
+
+Note:  You can use the "!!" symbol in front of an operation to evaluate it in-place.
+
+                function printN(n){
+                        console.log(!!n);
+                }
+                printN(5);              //<---- true
+                printN(NaN);            //<---- false
+                
+                //also:
+                
+                let n = 4, p = 5;
+                if (p%2) console.log("This is even");
+                if (!(n%2)) console.log("This is even");
         
 */
 // An illustration:
@@ -19,8 +34,20 @@ A Falsey value is a value that translates to false when evaluated in a Boolean c
 // You might normally do this:
 
                 function countdown(n){
-                    while(n>0){
-                    
-                    
+                        while(n>0){
+                                console.log(n);
+                                n--;
+                        }
                 }
+                console(5);
+
+// using Truthy/Falsey, you can also do this:
+
+                function countdown(n){
+                        while(n){                       //<---- This performs exactly the same way
+                                console.log(n);         //      N is true until it hits 0
+                                n--;                    //<---- As a point of reference, if n was a negative
+                        }                               //      number it would go on forever, so you have to be careful.
+                }
+                console(5);
 
