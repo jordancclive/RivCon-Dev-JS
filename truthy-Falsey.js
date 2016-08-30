1ae07f81392853f7b6c9cf01031c00d9b354f8fa
Vince <vince@riversconsulting.com>
2016-08-30 15:32:35 -0400

    Update truthy-Falsey.js

--- truthy-Falsey.js
+++ truthy-Falsey.js
@@ -15,6 +15,7 @@ A Falsey value is a value that translates to false when evaluated in a Boolean c
         - NaN
 
 Note:  You can use the "!!" symbol in front of an operation to evaluate it in-place.
+*/
 
                 function printN(n){
                         console.log(!!n);
@@ -28,7 +29,7 @@ Note:  You can use the "!!" symbol in front of an operation to evaluate it in-pl
                 if (p%2) console.log("This is even");
                 if (!(n%2)) console.log("This is even");
         
-*/
+        
 // An illustration:
 
 // You might normally do this:
@@ -50,4 +51,35 @@ Note:  You can use the "!!" symbol in front of an operation to evaluate it in-pl
                         }                               //      number it would go on forever, so you have to be careful.
                 }
                 console(5);
+
+
+// some examples of truthy/falsey:
+
+                !!5                     //<---- truthy and evaluates to true
+                !!(4%2)                 //<---- falsey & evaluates to false
+                !!(undefined)           //<---- falsey & evaluates to false
+                !!("a".length-1)        //<---- falsey & evaluates to false
+
+
+// some examples of this concept with And/Or Operators:
+
+//      Note:  the system looks at the first operator and if it sees "false", it can stop looking. If it
+//             sees something that is true, then it has to evaluate the second operator to determine if this
+//             expression evaluates to true or false.   
+//      ......"And" - Both operators must evaluate to true for the expression to evaluate "truthy":
+
+                1 && 1          //<----true
+                1 && 0          //<----false
+                0 && 1          //<----false
+                0 && 0          //<----false
+        
+//      ......"Or" - At least one operator must evaluate to true for the expression to evaluate "truthy":
+
+                1 || 1          //<----true
+                1 || 0          //<----true
+                0 || 1          //<----true
+                0 || 0          //<----false 
+
+
+
 
