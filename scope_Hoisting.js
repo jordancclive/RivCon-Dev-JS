2b9fcf2e2582ad11f993adf33691612f3b656361
Vince <vince@riversconsulting.com>
2016-08-30 14:05:40 -0400

    Update scope_Hoisting.js

--- scope_Hoisting.js
+++ scope_Hoisting.js
@@ -6,9 +6,13 @@ Inside a function:  Local Scope is defined as the parameters given to a function
 Global Scope is the highest order scope.  Any variable defined here is available to any piece of code.  
 These variables take on more memory because the engine itself needs to hold on to this information all the time.
 
+Global Scope:  The "window" of your web bvrowser has a scope.  This contains the "prototype" chain. ("TB Discussed Later").
+Variables declared without the "var" keyword are attached to this global scope.
+
 */
 
 
+//======================
 // Hoisting:  JavaScript does a couple of passes on the code before executing it.
 
 // Before code:
@@ -35,6 +39,7 @@ These variables take on more memory because the engine itself needs to hold on t
               // ****   Again remember that variable assignments/initialization are different than the declaration. **** 
 
 
+//======================
 // Back to Scope:
 
 // A function can access all of the variables of its parent code (and grandparent code and above that), but 
@@ -59,8 +64,71 @@ These variables take on more memory because the engine itself needs to hold on t
               console.log(x);                 //<---- you will get a reference error since x has not been defined.
 
 
-// With Scope in mind, let's talk about declaring variables using "let":
-/*
+// With Scope in mind, let's talk about declaring variables using "let":    (note: JavaScript 1.7 introduced "let")
 
-JavaScript 1.7 introduced "let".  
+// Let's start with an illustration:
 
+              function someStuff(){
+                for(var i=0; i<10; i++){
+                  //some stuff will happen
+                }
+                
+                console.log(i);                     //<---- you will get i=10 here.  You do not want this variable to
+                                                    //      exist at all here.  You are done with it after that loop ended.
+                for(var i=0; i<10; i++){            //<---- remember because of hoisting, you are now declaring the same 
+                  //some more stuff will happen     //      variable twice. (bad)
+                }
+              }
+
+// Let's make it a bit more complex to illustrate the issue
+
+              function someStuff(){
+                var arr = [];
+                for(var i=0; i<10; i++){
+                  arr.push(function(){                      //<----- You are creating an array of functions that will return i
+                    return i;                               //       Note: i is incrementing as it pushes these functions into
+                  })                                        //       the array.
+                }
+                for (var j=0; j<arr.length; j++)
+                console.log(arr[j]());                     //<---- Note: This array is an array of functions so you need
+              }                                            //      to invoke the function to get a value here.
+              
+              /* Here is what you would get as a result:
+              10          
+              10                This is a result of function scoping
+              10                using the "var" keyword
+              10
+              10
+              10
+              10
+              10
+              10
+              10
+              */
+              
+// If we changed to using the "let" keyword:
+
+              function someStuff(){
+                var arr = [];
+                for(let i=0; i<10; i++){
+                  arr.push(function(){                      //<----- You are creating an array of functions that will return i
+                    return i;                               //       Note: i is incrementing as it pushes these functions into
+                  })                                        //       the array.
+                }
+                for (var j=0; j<arr.length; j++)
+                console.log(arr[j]());                     //<---- Note: This array is an array of functions so you need
+              }                                            //      to invoke the function to get a value here.
+              
+              /* Here is what you would get as a result:
+              0          
+              1            
+              2  
+              3
+              4
+              5
+              6
+              7
+              8
+              9
+              */
+              
