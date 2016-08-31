699519ea28eacb3d8c1062dcf4ddfbe3d4e70195
Vince <vince@riversconsulting.com>
2016-08-31 14:57:08 -0400

    Update scope_Hoisting.js

--- scope_Hoisting.js
+++ scope_Hoisting.js
@@ -2,12 +2,15 @@
 
 /* Scope is the set of variables you can access at any given time.
 
-Inside a function:  Local Scope is defined as the parameters given to a function; "var" statements within the function.
+Inside a function:  Local Scope (or function scope) is defined as the parameters given to a function; "var" statements within the function.
+
 Global Scope is the highest order scope.  Any variable defined here is available to any piece of code.  
 These variables take on more memory because the engine itself needs to hold on to this information all the time.
 
-Global Scope:  The "window" of your web bvrowser has a scope.  This contains the "prototype" chain. ("TB Discussed Later").
+Global Scope:  The "window" of your web browser has a scope.  This contains the "prototype" chain. ("TB Discussed Later").
 Variables declared without the "var" keyword are attached to this global scope.
+
+Block scope {between the brackets} is scope only between two brackets.
 
 */
 
