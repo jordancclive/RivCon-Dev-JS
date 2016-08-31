1e3d8af37cdf346b7dc10bf5d8046982c4d6f027
Vince <vince@riversconsulting.com>
2016-08-31 11:31:04 -0400

    Update objects-Examples- CoreyParty.js

--- objects-Examples- CoreyParty.js
+++ objects-Examples- CoreyParty.js
@@ -3,18 +3,18 @@
 
 //This is my code.....use truthy/falsey; scan through an array and build a result object.
 
-              let coreysFriends = ['corey', 'corey', 'mom', 'corey','corey', 'dad', 'joe'];
-              
-              function countFriends(arr){
-              	let resultObj = {};
-              	for(let i=0; i<arr.length; i++){
-              		if(i){
-              			if (arr[i] in resultObj) resultObj[arr[i]] += 1;
-              			else resultObj[arr[i]] = 1
-              		}else resultObj[arr[i]] = 1;
-              	}
-              	return resultObj;
-              }
+      let coreysFriends = ['corey', 'corey', 'mom', 'corey','corey', 'dad', 'joe'];
+      
+      function countFriends(arr){
+      	let resultObj = {};
+      	for(let i=0; i<arr.length; i++){
+      		if(i){
+      			if (arr[i] in resultObj) resultObj[arr[i]] += 1;
+      			else resultObj[arr[i]] = 1
+      		}else resultObj[arr[i]] = 1;
+      	}
+      	return resultObj;
+      }
 
 //----------------------------------------
 // Corey's (instructor) solution follows:
@@ -24,15 +24,15 @@
 // ----> obj[currentName] = ++obj[currentName] || 1; <----
 // 
 
-function count(arr){
-	let obj = {};
-	debugger;
-	for(let i = 0; i < arr.length; i++){
-		let currentName = arr[i];
-		obj[currentName] = ++obj[currentName] || 1;	//<---- the first time through obj(currentName) is undefined
-	}							//      As a result, this truthy expression increments an 
-	console.log(obj);					//      undefined value (getting undefined) and the system 
-}								//      takes the 1 (starting a key/value pair)
-console.log(countFriends(coreysFriends));			//      subsequent iterations use the ++ to increment the 
-console.log(count(coreysFriends));				//      value when currentName exists within the object.
-"";
+	function count(arr){
+		let obj = {};
+		debugger;
+		for(let i = 0; i < arr.length; i++){
+			let currentName = arr[i];
+			obj[currentName] = ++obj[currentName] || 1;	//<---- the first time through obj(currentName) is undefined
+		}							//      As a result, this truthy expression increments an 
+		console.log(obj);					//      undefined value (getting undefined) and the system 
+	}								//      takes the 1 (starting a key/value pair)
+	console.log(countFriends(coreysFriends));			//      subsequent iterations use the ++ to increment the 
+	console.log(count(coreysFriends));				//      value when currentName exists within the object.
+	"";
