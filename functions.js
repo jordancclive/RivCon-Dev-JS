cbb0c5c9ff89bbf6aeecfc5864ab173eb162796c
Vince <vince@riversconsulting.com>
2016-08-31 15:37:26 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 /* Functions.
 
 Functions that return other functions are called "closures".  
-What "closures" is about is ---->  keeping variables in "active" memory.
+What "closure" is about is ---->  keeping variables in "active" memory.
 
 ==============
 Some premises:
@@ -24,5 +24,28 @@ Some examples:
                 }
                 return childFunction();                       //notice you are invoking this function meaning
               }                                               // that the result passed is this function executed.
-              
+
               parentFunction();                               //---->so you will get:  Today is sunny
+
+
+
+// Let's not invoke this function, but simply return it......
+
+              function parentFunction(weather){
+                function childFunction(){
+                  console.log("Today it is " + weather);
+                }
+                return childFunction;                         //now you are not returning the function
+              }                                               
+              let hold = parentFunction('sunny');
+              let hold2 = parentFunction('rainy');  
+              console.log(typeOf hold);                       //<---- this variable is a  "function" 
+              console.log(typeOf hold()).                     //<---- This is "undefined"
+              hold2();                                        //---->you will get:  Today is rainy
+              hold();                                         //---->you will get:  Today is sunny
+              
+              // This example shows "closure"
+              //What is happening is that the variable "hold" representing the "childFunction".
+              //When you individually invoke "hold" it is remembering the variables that was passed to 
+              // "childFunction" from the "parentFuncton".
+              // So the childFunction is responsible for remembering the parent's original values when you pass it.
