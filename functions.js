1c7b88bf28da2719e3791cd119af554c069a9ca8
Vince <vince@riversconsulting.com>
2016-08-31 15:14:26 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,4 +3,25 @@
 Functions that return other functions are called "closures".  
 What "closures" is about is ---->  keeping variables in "active" memory.
 
+==============
 Some premises:
+==============
+
+- functions have access to variables in their parent scope.
+
+- functions "know" their environment.
+
+
+==============
+Some examples:
+==============
+
+              function parentFunction(){
+                var weather = 'sunny';
+                function childFunction(){
+                  console.log("Today it is " + weather);
+                }
+                return childFunction();                       //notice you are invoking this function meaning
+              }                                               // that the result passed is this function executed.
+              
+              parentFunction();                               //---->so you will get:  Today is sunny
