538293cb25d8b7977247025fe68cf2fa57fe066d
Vince <vince@riversconsulting.com>
2016-08-31 11:47:27 -0400

    Update objects-Examples-CoreyParty.js

--- objects-Examples-CoreyParty.js
+++ objects-Examples-CoreyParty.js
@@ -32,7 +32,7 @@
     			}                                               //      As a result, this truthy expression increments an
     			return obj;                                     //      undefined value (getting undefined) and the system 
     			}                                               //      takes the 1 (starting a key/value pair)
-    			console.log(countFriends(coreysFriends));       //      subsequent iterations use the ++ to increment the 
-    			console.log(count(coreysFriends));              //      value when currentName exists within the object.
+    	console.log(countFriends(coreysFriends));                   //      subsequent iterations use the ++ to increment the 
+    	console.log(count(coreysFriends));                          //      value when currentName exists within the object.
     	"";
 
