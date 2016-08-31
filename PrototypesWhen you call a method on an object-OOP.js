a30fb176714a0f5ba81d24a8e5be5159a6a1d77e
Vince <vince@riversconsulting.com>
2016-08-31 16:53:05 -0400

    Create PrototypesWhen you call a method on an object-OOP.js

---
+++ PrototypesWhen you call a method on an object-OOP.js
@@ -0,0 +1,14 @@
+// Prototyping-OOP
+/*
+When someone says "Object Oriented" they mean something dependent on objects.  
+JavaScript is a "functional" language, but has a lot of object oriented aspects to it.
+
+When you call a method on an object and the method uses the keyword "this", "this" refers to the object you're 
+calling the method on (the variable before the dot).
+*/
+
+//------------------
+// An example:
+
+var me = {
+
