902103e388c713897c73b9ed4ee7a95111177210
Vince <vince@riversconsulting.com>
2016-09-01 10:59:19 -0400

    Create x-Obj-coreyParty.js

---
+++ x-Obj-coreyParty.js
@@ -0,0 +1,40 @@
+// Corey's Party - Listing the attendees in an object and counting people with the same first name.
+// correct answer:  {corey: 4, mom: 1, dad: 1, joe: 1}
+
+//This is my code.....use truthy/falsey; scan through an array and build a result object.
+
+      let coreysFriends = ['corey', 'corey', 'mom', 'corey','corey', 'dad', 'joe'];
+      
+      function countFriends(arr){
+      	let resultObj = {};
+      	for(let i=0; i<arr.length; i++){
+      		if(i){
+      			if (arr[i] in resultObj) resultObj[arr[i]] += 1;
+      			else resultObj[arr[i]] = 1                            //here is code repeating with the next line
+      		}else resultObj[arr[i]] = 1;                                //this is why the Corey (instructor) used the
+      	}                                                                 //different strategy/method below
+      	return resultObj;
+      }
+
+//----------------------------------------
+// Corey's (instructor) solution follows:
+//Corey also uses truthy/falsey; scan through an array and build a result object.
+
+// interesting bit of code:
+// ----> obj[currentName] = ++obj[currentName] || 1; <----
+// 
+
+    	function count(arr){
+    		let obj = {};
+    		for(let i = 0; i < arr.length; i++){
+    		      let currentName = arr[i];
+    		      obj[currentName] = ++obj[currentName] || 1;     //<---- the first time through obj(currentName) is undefined
+    		}                                                     //      As a result, this truthy expression increments an
+    		return obj;                                           //      undefined value (getting undefined) and the system 
+    	}                                                           //      takes the 1 (starting a key/value pair)
+    	console.log(countFriends(coreysFriends));                   //      subsequent iterations use the ++ to increment the 
+    	console.log(count(coreysFriends));                          //      value when currentName exists within the object.
+    	""; 
+    	
+    	//Note: The code:   ++obj[currentName]   can also be written ....>  obj[currentName] + 1
+
