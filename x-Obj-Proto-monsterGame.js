362c1dc7647143b0b752a4a389ac6e717e83feb9
Vince <vince@riversconsulting.com>
2016-09-01 20:39:34 -0400

    Update x-Obj-Proto-monsterGame.js

--- x-Obj-Proto-monsterGame.js
+++ x-Obj-Proto-monsterGame.js
@@ -1,1 +1,20 @@
 // The Monster Game (simple)
+/*
+    assumptions:
+    
+    want a class called monsters.  They will has health, attack and defence.
+    want 2 methods:
+      
+      - restore health resets health to 100 or full health
+      - get attacked:  takes in an attack as a parameter
+                       and when a monster gets attacked does some calculation
+                       with monsters defence and makes some change to health.
+      
+      Ex: (4* attack from person attacking) - (2* defence of monster being attacked)
+          gets subtracted from health.
+    
+    going to have a monster constructor
+    2 methods
+*/
+
+function 
