931d116b97dd3254b1c076a08b8a5fd9dd22b100
Vince <vince@riversconsulting.com>
2016-09-05 12:39:19 -0400

    Update functions-global.js

--- functions-global.js
+++ functions-global.js
@@ -2,14 +2,27 @@
 /* rather than on an object:
 
     
-    eval()            //<---- *** never use this one. ***
+eval()      //<---- *** never use this one. ***
     
-          // taked a string as an argument and LITERALLY runs it as code.  
-          // If an external party knows you have this on your server, then they can 
-          // run their own code on your server.
+            // taked a string as an argument and LITERALLY runs it as code.  
+            // If an external party knows you have this on your server, then they can 
+            // run their own code on your server.
         
-          An example:
-          let str = "console.log('hi');"
-          eval(str);    //<---- returns:  hi */
+            An example:
+            let str = "console.log('hi');"
+            eval(str);    //<---- returns:  hi */
           
-          
+    // ----------------------  
+     
+isNaN()
+    
+    // If you typed:
+    
+    console.log(typeof NaN);  //<--- returns: number
+    
+    // As a result, isNaN() was created.
+    
+    isNaN(5);   // returns: false
+    isNaN(NaN); // returns: true
+    
+
