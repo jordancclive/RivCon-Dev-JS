0283b257071dcf9b5ef0c3554f46c47f7444f725
Vince <vince@riversconsulting.com>
2016-09-05 16:23:49 -0400

    Create classMethods-Math.js

---
+++ classMethods-Math.js
@@ -0,0 +1,25 @@
+//classMethods-Math.js
+
+// These are accessed with the Math Object directly...
+
+      Math.abs(num);        // makes the number positive
+
+      Math.round(num);
+      
+      Math.floor(num);
+      
+      Math.ceil(num);
+      
+      Math.max(numbers...);
+      
+      Math.min(numbers...);
+      
+      Math.random();        // generates a random number between 0 & 1.
+      
+                            //let's get a number between 1 & 10
+                            
+                            console.log(Math.round(Math.random() *9)+1);
+                            
+                            // This is not really random....based on system time.
+      
+      
