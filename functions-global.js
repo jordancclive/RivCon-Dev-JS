4ff90c9c1731a760a54dd7bc7bc75f2b49c06a70
Vince <vince@riversconsulting.com>
2016-09-05 14:31:34 -0400

    Update functions-global.js

--- functions-global.js
+++ functions-global.js
@@ -11,8 +11,9 @@ eval()      //<---- *** never use this one. ***
             An example:
             let str = "console.log('hi');"
             eval(str);    //<---- returns:  hi */
+
           
-    // ----------------------  
+// ----------------------  
      
 isNaN()
     
@@ -22,7 +23,32 @@ isNaN()
     
     // As a result, isNaN() was created.
     
-    isNaN(5);   // returns: false
-    isNaN(NaN); // returns: true
+    isNaN(5);       // returns: false
+    isNaN(NaN);     // returns: true
     
+    
+// ----------------------  
+
+parseInt();         // turn something into an integer
+
+parseFloat();       // turn something into a "double" from a different data type.
+
+typeof Number('5')  // returns a number
+
+
+// ---------------------- 
+
+// Environment dependent Global functions:
+
+// There exists a global object called: "console" havig the method of log() and dir() in our environment.
+
+// ...be aware if you are in node or mozilla or chrome, they all are environments that may have some
+// unique global variables to each of them.
+
+//....when using these buit-in object use documentation:
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
+
+
+// ---------------------- 
 
