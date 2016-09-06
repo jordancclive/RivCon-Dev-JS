148afa409eaf8f16751e71fbcc72bcd2265b330e
Vince <vince@riversconsulting.com>
2016-09-06 14:02:35 -0400

    Update scope_Hoisting.js

--- scope_Hoisting.js
+++ scope_Hoisting.js
@@ -12,6 +12,29 @@ Variables declared without the "var" keyword are attached to this global scope.
 
 Block scope {between the brackets} is scope only between two brackets.
 
+-------------------------
+
+	We need to keep in mind hoisting within the function...
+	
+    	Action 1: functions within are hoisted to the top of this function (They will be at the top - 1st)
+    	Action 2: variable declarations are then hoisted underneath the functions (They will be second)
+    	
+    	Note 1: variable assignments are made in place.  (They stay where they are in the code)
+    	Note 2: The function call runs in order.
+    	Note 3: Functions declared as variables & IIFEs follow Action 2 & Note 1.
+    	
+    	Footnotes: 
+    	
+    		1. 	These nested functions (below) are IIFEs 
+    			(immediately-invoked function expressions)
+    	
+    		2. 	So they are not going to be hoisted and will run in order where they are placed in the parent function.
+    		   
+    		3. 	When a variable is passed in as an argument, 
+      			the parameter is a variable local to the nexted function. namely: where the 
+      			variable assignment is made.  If the variable exists globally, the global value is, 
+      			in essence, a different variable.
+
 */
 
 
