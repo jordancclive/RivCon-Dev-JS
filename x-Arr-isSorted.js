36d9a40dc7b7733d805625370cc872fefa063947
Vince <vince@riversconsulting.com>
2016-09-06 16:19:59 -0400

    Create x-Arr-isSorted.js

---
+++ x-Arr-isSorted.js
@@ -0,0 +1,22 @@
+/*
+Check if Array is Sorted
+Complete the function isSorted, which takes an array as inputed and outputs a boolean indicating if the array's contents are in ascending order.
+
+    isSorted([8,2,3,4])
+    // -> false
+
+
+    isSorted([2,3,4,8])
+    // -> true
+
+*/
+        function isSorted(arr) {
+        	for(let i=0; i< arr.length-1; i++){
+        		if(arr[i] > arr[i+1]) return false
+        	}
+        	return true;
+        }
+        
+        // tests follow:
+        console.log(isSorted([8,2,3,4]));  
+        console.log(isSorted([2,3,4,8])); 
