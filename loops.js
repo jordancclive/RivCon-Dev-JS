cba7a795b605fc4f364bf2b215d44e7a607bef31
Vince <vince@riversconsulting.com>
2016-09-07 13:46:02 -0400

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,21 @@
+// Summary discussion on loops....
+
+
+//-------------------------------
+/*For loops:
+
+Remember to use them when you have a set amount of things to iterate through.  
+Have a starting & ending position.  Typically use this when you have a flat datatype like a string or an array.
+Most of the time you will be using .length (there are exceptions to this).
+
+The structure is:
+
+    for ( initialize the loop ; chk to continue the loop ; after each iteration update the loop ) 
+                  { some code to do stuff };
+*/
+    for(let i=0, i< arr.length, I++){
+        console.log('hello!')
+    };
+
+
+//-------------------------------
