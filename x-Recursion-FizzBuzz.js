55e99a862518db86dd0e4f897404f749b693bd3d
Vince <vince@riversconsulting.com>
2016-09-06 20:13:35 -0400

    Create x-Recursion-FizzBuzz.js

---
+++ x-Recursion-FizzBuzz.js
@@ -0,0 +1,31 @@
+//Complete Reverse FizzBuzz()
+//The function should accept a number n, and console.log() all the numbers between 1-n. Any numbers divisible by 3 should say "Fizz", numbers divisible  by 5 should say "Buzz" and numbers divisble by 15 should say 'Fizzbuzz'
+
+//eg FizzBuzz(16) => 16, FIZZBUZZ, 14, 13, FIZZ, 11, BUZZ, FIZZ, 8, 7, FIZZ, BUZZ, 4, FIZZ, 2, 1 
+
+
+
+
+
+
+
+var FizzBuzz = function(n) {
+	let result = "";
+	if(n === 0) console.log('Done!');
+	else{
+		if (n%3 === 0) result = "FIZZ";
+		if (n%5 === 0) result += "BUZZ";
+		if (result === "") result = n.toString();
+		console.log(result);
+		FizzBuzz(n-1);
+	} 
+	return
+};
+
+
+
+
+
+
+//tests
+FizzBuzz(100);
