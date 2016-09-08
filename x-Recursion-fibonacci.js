1210b4e94444d4a356c12dc099f9d81d15dece08
Vince <vince@riversconsulting.com>
2016-09-07 20:58:41 -0400

    Update x-Recursion-fibonacci.js

--- x-Recursion-fibonacci.js
+++ x-Recursion-fibonacci.js
@@ -28,3 +28,26 @@ console.log(fib(5)); // ===> 5);  //true
 console.log(fib(7)); // ===>13);	
 console.log(fib(9)); // ===> 34);
 console.log(fib(11)); // ===> 89);
+
+
+//----------------------------------
+
+//Teacher's result:
+
+		fib(1) // 1
+		fib(0) // 0
+		
+		//base case
+		//recursive case
+*/
+
+var fib = function(n) { //5
+	// if(n===1) return 1;
+	// if(n===0) return 0;
+	if(n <= 1) return n;
+	return fib(n-1) + fib(n-2);    //wow, I would have never thought of this one!!
+	
+}
+
+console.log(fib(5));
+console.log(fib(9));
