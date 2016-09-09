52d06a325a108e764001893b6d76d15deeb3650b
Vince Rios <vince@riversconsulting.com>
2016-09-09 16:22:54 -0400

    Update recursion.js

--- recursion.js
+++ recursion.js
@@ -36,8 +36,31 @@ countdown(4);
     Step 2: Determine the recursive case.  ...when/why should you have a finction call itself.
     
     Step 3: Then modify the code as necessary.
-    
-    
+*/
+
+// Simple factorial example:
+
+/*
+	FACTORIAL 
+	
+	Define a function factorial() that takes a number n as input and returns n factorial
+	
+	Factorial = the product of all the numbers between n and 1. The factorial of 0 is 0
+	
+	n! = n * n-1 * n-2 ..... * 2 * 1;
+*/
+
+var factorial = function(n) {
+	if (n<=1) return 1;
+	return n * factorial(n-1);
+}
+
+
+//Tests
+console.log(factorial(0) === 1)
+console.log(factorial(2) === 2);
+console.log(factorial(5) === 120 );
+console.log(factorial(10) === 3628800);     // returns:  true for all of these.
     
     
     
