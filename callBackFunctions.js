e94d1c8e69c27b5d7aeb8dd527aab477aaa75417
Vince Rios <vince@riversconsulting.com>
2016-09-13 16:30:51 -0400

    Update callBackFunctions.js

--- callBackFunctions.js
+++ callBackFunctions.js
@@ -10,6 +10,32 @@
 An example:
 */
 
+// This is an overseer function.  it takes in a variable and a function as inputs.
+// It then returns the function invoked with the variable as its input.
+
     function overseer(num, operatorFunc){
         return operatorFunc(num);
     }
+
+//-------------------------------------------
+
+// here are 2 plain functions doing simple tasks:
+
+    function addTwo(num){
+      return num+2;
+    }
+    
+    function multiplyByTwo(num){
+      return 2*num;
+    }
+
+//-------------------------------------------
+
+// notice we are not invoking the 2 functions sent as arguments, just the name of the function.
+
+    console.log(overseer(10,addTwo));           //returns: 12
+    console.log(overseer(10, multiplyByTwo));   //returns 20
+
+//-------------------------------------------
+
+
