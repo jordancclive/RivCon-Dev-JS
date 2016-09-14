a4c729109d42607064ee554724767c0e8ac674ba
Vince Rios <vince@riversconsulting.com>
2016-09-13 20:56:19 -0400

    Create x-g-poetryGenerator.js

---
+++ x-g-poetryGenerator.js
@@ -0,0 +1,17 @@
+// Project:  build a poetry generator
+
+/*
+Generator will do 2 things:
+    1. Generate something complete based on a word corpus (no fill-in-the-blanks)  (???)
+    2. Train itself to write poetry based on a Markov Chain.                       (???)
+
+Our poetry generator will consume a corpus text it will model it's output poetry on. 
+Create a function that parses a multiline string and returns an array of the words in the string, 
+uniformly formatted with no numbers or punctuation.
+
+You may accomplish this using a series of loops or with a regular expression 
+see----> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+
+
+
+*/
