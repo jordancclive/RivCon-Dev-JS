b6b937f28cf41d8640efb5d2c0cf7b41ca0233d7
Vince Rios <vince@riversconsulting.com>
2016-09-18 16:42:47 -0400

    Update callBackFunctions.js

--- callBackFunctions.js
+++ callBackFunctions.js
@@ -1,10 +1,10 @@
-// Call Back Functions
+// Call Back Functions - take in functions as a parameter.
 
 /*
   A high order language is one where all datatypes including functions has the ability 
   to be passed as inputs and outputs.
   
-  We will be talking about array methods that take in functions as arguments....
+  We will be talking about 5 array methods that take in functions as arguments....
   
   
 An example:
@@ -39,8 +39,8 @@ An example:
 //-------------------------------------------
 
 
-// What follows are built-in array methods that expect custom callback function as an argument.
-
+// What follows are 5 built-in array methods that expect custom callback function as an argument.
+/*
 
 .forEach
 
@@ -53,5 +53,36 @@ An example:
 .sort
 
 //-------------------------------------------
+
+A .forEach is a fancy wrapper for a for loop that is dealing with an array. It is a very generalized for loop with no return value.
+Its return value is undefined.
+
+      - When a .forEach executes,it automatically performs some action on each element.
+      
+      - It is a method that takes in an anonymous function (the callback function) as its parameter.
+      
+      - The arguments of this functions are:  value (an element of the array) & index of the array.
+      
+      An example:
+*/
+//-----------------------------------------------
+
+      var num = 1;
+      var arr = [1,2,3];
+      
+      arr.forEach(function(val, ind){         // As you can see this satement is wrtten showing .forEach as a method
+                                              // passing an anonymous function and the 2 parameters to it.
+        num *= val;                           // The function is doing one thing automatically to every element
+                                              // of the array.  ...that is: num *= val;
+      });
+      
+      console.log(num);     //result: 6
+                                              // Once the function is finished the value num has now changed.
+                                              
+//-----------------------------------------------                                           
+      
+
+
+
 
 
