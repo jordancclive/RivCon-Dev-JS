82fa9dd7fcd51fd0a2e32d543c185cc297ebf5dd
Vince Rios <vince@riversconsulting.com>
2016-09-19 15:52:53 -0400

    Create x-g-histogram.js

---
+++ x-g-histogram.js
@@ -0,0 +1,38 @@
+/*A 6-sided dice is rolled a number of times and the results are plotted as a character-based histogram.
+
+Example:
+
+6|##### 5
+5|
+4|# 1
+3|########## 10
+2|### 3
+1|####### 7
+Task
+
+You will be passed all the dice roll results, and your task is to write the code to return a string 
+representing a histogram, so that when it is printed it has the same format as the example.
+
+Notes
+
+There are no trailing spaces on the lines
+All lines (including the last) end with a newline \n
+A count is displayed beside each bar except where the count is 0
+The number of rolls may vary but there are never more than 100
+*/
+
+function histogram(results) {
+	resultStr = '';
+	results.reverse();
+	results.forEach(function(elem, index){
+		resultStr += (6 - index) + '|';
+		for (let i = 0; i< elem; i++){
+			resultStr += '#';
+		}
+		if (elem) resultStr += ' ' + elem + '\n';
+		else resultStr += '\n';
+	});
+  return resultStr;
+}
+
+console.log(histogram([7,3,10,1,0,5]));
