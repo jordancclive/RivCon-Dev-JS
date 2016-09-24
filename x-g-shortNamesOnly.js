fe1b1f4ea69d3e25ec8703bcfa602616db335d41
Vince Rios <vince@riversconsulting.com>
2016-09-24 14:43:56 -0400

    Update x-g-shortNamesOnly.js

--- x-g-shortNamesOnly.js
+++ x-g-shortNamesOnly.js
@@ -12,4 +12,45 @@ function shortNamesOnly(arr){
 	})
 	
 }
-console.log(shortNamesOnly(people));
+console.log(shortNamesOnly(people));		// result:  [ { name: 'doug', age: 25 }, { name: 'vin', age: 60 } ]
+
+
+//the above callback is performing an "operation" or "evaluation" a property and therefor returns the entire selected object.
+// below we are returning a certain key/value pair.  The one that is named.
+
+//------------------------------------------------
+
+// namesOnly
+/*
+Take an array of objects (elements = people), return the ones whose names are <= 4 in length.
+*/
+
+let people = [
+	{name: 'doug', age: 25}, {name: 'vin', age: 60}, {name: 'Sandra', age: 40}, {name: 'diana', age: 30}];
+	
+function namesOnly(arr){
+	return arr.map(function(elem,index){
+		return elem.name;
+	})
+	
+}
+console.log(namesOnly(people));		// result: [ 'doug', 'vin', 'Sandra', 'diana' ]
+
+
+//------------------------------------------------
+
+// agesOnly
+/*
+Take an array of objects (elements = people), return the ones whose names are <= 4 in length.
+*/
+
+let people = [
+	{name: 'doug', age: 25}, {name: 'vin', age: 60}, {name: 'Sandra', age: 40}, {name: 'diana', age: 30}];
+	
+function agesOnly(arr){
+	return arr.map(function(elem,index){
+		return elem.age;
+	})
+	
+}
+console.log(agesOnly(people));		// result: [ 25, 60, 40, 30 ]
