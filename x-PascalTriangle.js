594221e4710becf5265a561da8c03d2790b8c6dc
Vince Rios <vince@riversconsulting.com>
2016-09-25 23:25:30 -0400

    Update x-PascalTriangle.js

--- x-PascalTriangle.js
+++ x-PascalTriangle.js
@@ -60,3 +60,32 @@ function pascalsTriangle(n) {
 	}
 	return newArr;
 }
+
+
+//------------------------------------------------
+//Other cool answers:
+
+				function pascalsTriangle(n) {
+				  var pascal = [];
+				  var idx = 0;
+
+				  for ( var i = 0; i < n; i++ ) {
+				    idx = pascal.length - i;
+				    for ( var j = 0; j < i+1; j++ ) {
+				      if ( j === 0 || j === i ) {
+					pascal.push(1);
+				      } else {
+					pascal.push( pascal[ idx+j ] + pascal[ idx+j-1 ] );
+				      }
+				    }
+				  }
+
+				  return pascal;
+				}
+
+
+//----------------------------------------------------
+
+
+
+
