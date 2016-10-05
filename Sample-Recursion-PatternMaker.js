dcd4d45d8be0297fad1d9768d2e23ba506027809
Vincent Rios <vince@riversconsulting.com>
2016-10-05 16:19:31 -0400

    Create Sample-Recursion-PatternMaker.js

---
+++ Sample-Recursion-PatternMaker.js
@@ -0,0 +1,19 @@
+/*
+	Pattern Master
+	
+	create a function patternMaker() that takes two arguments: an integer n and a piece of data of any type. patternMaker() should return an array of the second argument, repeated n times
+	
+	ex)
+		patternMaker(5, 'Boo') --> ['Boo', 'Boo', 'Boo', 'Boo', 'Boo']
+		patternMaker(3, ['cat', 'dog']) --> [['cat', 'dog'], ['cat', 'dog'], ['cat', 'dog']]
+*/
+
+function patternMaker(n, item) {
+	let resultArr = [];
+	resultArr.push(item);
+	if(n!==1) {
+		resultArr = resultArr.concat(patternMaker(n-1, item));
+	}
+	return resultArr;
+}
+patternMaker(3, ['cat', 'dog']);
