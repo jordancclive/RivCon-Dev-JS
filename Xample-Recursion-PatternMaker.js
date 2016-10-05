ba85536da8d5c9bf55df1420443f9ee4c55add5c
Vincent Rios <vince@riversconsulting.com>
2016-10-05 16:24:44 -0400

    Update Xample-Recursion-PatternMaker.js

--- Xample-Recursion-PatternMaker.js
+++ Xample-Recursion-PatternMaker.js
@@ -1,19 +1,25 @@
-/*
-	Pattern Master
+/*	Pattern Master
 	
 	create a function patternMaker() that takes two arguments: an integer n and a piece of data of any type. patternMaker() should return an array of the second argument, repeated n times
 	
 	ex)
 		patternMaker(5, 'Boo') --> ['Boo', 'Boo', 'Boo', 'Boo', 'Boo']
 		patternMaker(3, ['cat', 'dog']) --> [['cat', 'dog'], ['cat', 'dog'], ['cat', 'dog']]
-*/
+ 
+The recursive solution:	*/
 
-function patternMaker(n, item) {
-	let resultArr = [];
-	resultArr.push(item);
-	if(n!==1) {
-		resultArr = resultArr.concat(patternMaker(n-1, item));
-	}
-	return resultArr;
-}
-patternMaker(3, ['cat', 'dog']);
+		function patternMaker(n, item) {
+			let resultArr = [];
+			resultArr.push(item);
+			if(n!==1) resultArr = resultArr.concat(patternMaker(n-1, item));
+			return resultArr;
+		}
+		patternMaker(3, ['cat', 'dog']);
+
+//The non-recursive solution:
+
+		function patternMaker(n, item) {
+			var returnArr = [];
+			for(var i = 0; i < n; i++) returnArr.push(item);
+			return returnArr;
+		}
