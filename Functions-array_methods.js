81b1fc0949f3918012984ee19f81d371d5f4b79d
Vincent Rios <vince@riversconsulting.com>
2016-10-06 10:21:01 -0400

    Update Functions-array_methods.js

--- Functions-array_methods.js
+++ Functions-array_methods.js
@@ -1,5 +1,18 @@
 // functions-array_methods.js
 
+              /*---------------
+                Arrays are a list of values ( a DATASET) between square brakets with elements separated by commas.
+                
+                          var arrayName = [];  defines an array.
+                          
+                Note: Strings will not let you do the following:
+                
+                          var myArray = [];
+                          myArray[0] = 50;
+                          console.log(myArray[0]);    //result: 50
+
+              */---------------
+
 Array.isArray(array);         // this is a static method.
 
 arr.concat(array);            // concatenate two instances of an array. returns the new array, not changing the originals.
@@ -27,7 +40,7 @@ arr.split(delimiter);
               
               let date = new Date();
               console.log(date);                              
-              // result:  Mon Sep 05 2016 15:26:52 GMT-0400 (EDT)   
+              // result:  Mon Sep 05 2016 15:26:52 GMT-0400 (EDT)     
               // need:    YYYYMMDDHHMMSS
               
               let dateArr = date.toString().split(' ');       
@@ -56,16 +69,111 @@ arr.split(delimiter);
               resultArr[3] = newHMS;
               let result = resultArr.join('');
               
-              console.log(result);
+              console.log(resultArr);   // returns: [ '2016', '10', '05', '224733' ]
+
+              //---------------
               
 arr.pop();
 
+              //---------------
+              /* remove values to the end of an array.
+              The pop method also 'returns' this value.
+              */
+
+              var myArray = [1,2,3,4,5,6];
+              var num = myArray.pop();
+
+              console.log(myArray);     //result: [1,2,3,4,5]
+              console.log(num);         //result: 6
+
+              //---------------              
+
+
 arr.push(value);
+
+              //---------------
+              /* Adds values to the end of an array.
+              The push method also 'returns' a new length of an array
+              */
+
+              let arrX = [1,2,3,4];
+              console.log('arrX.length:',arrX.length,'at start');   //returns: arrX.length: 4 at start
+              let len = arrX.push(5);
+              console.log('new arrX.length:', arrX.length);         //returns: new arrX.length: 5
+              console.log('new values:', arrX);                     //returns: new values: [ 1, 2, 3, 4, 5 ]
+
+              //---------------
 
 arr.reverse(from, to);
 
 arr.slice(from, to);
 
+              //---------------
+              /*   The following works for both strings and arrays
+
+                  syntax: str.slice(beginSlice[, endSlice])
+                  
+                  beginSlice: The zero-based index at which to begin extraction.
+                  endSlice:   Optional. The zero-based index at which to end extraction. 
+                              (does not include this stated index value)               
+                  
+                  syntax: str.substr(start [, length])
+                  
+                  start:    Location at which to begin extracting characters.
+                  length:   Optional. The number of characters to extract.
+*/
+
+              console.log([0,1,2,3,4].slice(2,4));              //result: [ 2, 3 ]
+              console.log([0,1,2,3,4].slice(2));                //result: [ 2, 3, 4 ]
+              console.log([0,1,2,3,4].slice());                 //result: [ 0, 1, 2, 3, 4 ]
+              console.log('VincentJosephRios'.slice(2,4));      //result: nc
+              console.log('VincentJosephRios'.slice(2));        //result: ncentJosephRios
+              console.log('VincentJosephRios'.slice());         //result: VincentJosephRios
+
+              console.log([0,1,2,3,4].substr(2,4));             //result: ERROR - does not work with arrays
+              console.log([0,1,2,3,4].substr(2));               //result: ERROR - does not work with arrays
+              console.log([0,1,2,3,4].substr());                //result: ERROR - does not work with arrays
+              console.log('VincentJosephRios'.substr(2,4));     //result: ncen
+              console.log('VincentJosephRios'.substr(2));       //result: ncentJosephRios
+              console.log('VincentJosephRios'.substr());        //result: VincentJosephRios
+
+              //---------------
+
 arr.sort();
 
 arr.splice(from, num);
+
+str.substr(start [, length])        (NOT AN ARRAY METHOD.  This is only for strings)
+
+              //---------------
+              /*   The following works for both strings and arrays
+
+                  syntax: str.slice(beginSlice[, endSlice])
+                  
+                  beginSlice: The zero-based index at which to begin extraction.
+                  endSlice:   Optional. The zero-based index at which to end extraction. 
+                              (does not include this stated index value)               
+                  
+                  syntax: str.substr(start [, length])
+                  
+                  start:    Location at which to begin extracting characters.
+                  length:   Optional. The number of characters to extract.
+              */
+
+              console.log([0,1,2,3,4].slice(2,4));              //result: [ 2, 3 ]
+              console.log([0,1,2,3,4].slice(2));                //result: [ 2, 3, 4 ]
+              console.log([0,1,2,3,4].slice());                 //result: [ 0, 1, 2, 3, 4 ]
+              console.log('VincentJosephRios'.slice(2,4));      //result: nc
+              console.log('VincentJosephRios'.slice(2));        //result: ncentJosephRios
+              console.log('VincentJosephRios'.slice());         //result: VincentJosephRios
+
+              console.log([0,1,2,3,4].substr(2,4));             //result: ERROR - does not work with arrays
+              console.log([0,1,2,3,4].substr(2));               //result: ERROR - does not work with arrays
+              console.log([0,1,2,3,4].substr());                //result: ERROR - does not work with arrays
+              console.log('VincentJosephRios'.substr(2,4));     //result: ncen
+              console.log('VincentJosephRios'.substr(2));       //result: ncentJosephRios
+              console.log('VincentJosephRios'.substr());        //result: VincentJosephRios
+
+              //---------------
+
+
