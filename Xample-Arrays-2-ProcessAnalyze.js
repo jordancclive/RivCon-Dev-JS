274b8d833776674595acd05b7b40ff1abdb10243
Vincent Rios <vince@riversconsulting.com>
2016-10-06 17:32:15 -0400

    Update Xample-Arrays-2-ProcessAnalyze.js

--- Xample-Arrays-2-ProcessAnalyze.js
+++ Xample-Arrays-2-ProcessAnalyze.js
@@ -2,46 +2,55 @@
     looking at similarities and differences between the 2 arrays.
 */
         
-        function process2Arrays(arr1, arr2) {
-          let bothArrays = 0, oneArray = 0, inArr1 = 0, inArr2 = 0;
-          let doneWithArr1=false;
-          //loop through Array 1
-          for(let i=0; i<arr1.length; i++){
-            //loop through Array 2
-            for(let j=0; j<arr2.length; j++){
+                        function process2Arrays(arr1, arr2) {
+                            /*
+                            Return an array with the following elements:
+                            (1) bothArrays:		Number of elements that are present in both arrays
+                            (2) oneArray:		Number of elements that are present in only one array
+                            (3) inArr1:			Number of remaining elements after extracting to arr1, 
+                                                    the elements that are present in arr2.
+                            (4) inArr2:			Number of remaining elements after extracting to arr2, 
+                                                    the elements that are present in arr1.
+                            */
+                            //setup these above variables...
+                            let bothArrays = 0, oneArray = 0, inArr1 = 0, inArr2 = 0;
+                            console.log('arr1', arr1);
+                            console.log('arr2', arr2);
 
+                            //First lets see if either/both array(s) = zero length...
+                            if(!arr1.length && !arr2.length) return [bothArrays, oneArray, inArr1, inArr2];
+                            else if(!arr1.length){
+                                oneArray = arr2.length;
+                                inArr2 = arr2.length;
+                                return [bothArrays, oneArray, inArr1, inArr2];
+                            }else if(!arr2.length) {
+                                oneArray = arr1.length;
+                                inArr1 = arr1.length;
+                                return [bothArrays, oneArray, inArr1, inArr2];		
+                            }else{
+                                //Let's loop through one of the arrays:
+                                for(let i=0; i<arr1.length; i++){
+                                    //Is the element in arr2?
+                                    if(arr2.indexOf(arr1[i])>=0) {
+                                        bothArrays+=1;
+                                    }else{
+                                        oneArray+=1;
+                                        inArr1+=1;
+                                    }
+                                }
 
-              console.log('Arr1:',arr1[i],'Arr2:',arr2[j]);
-              console.log('Before:      ','bothArrays:',bothArrays,'oneArray:',oneArray,'inArr1:',inArr1,'inArr2:',inArr2);
-              if(doneWithArr1){
-                  inArr2+=1;
-                  oneArray+=1;
-              }else{
-                if (arr1[i] === arr2[j]){
-                  bothArrays+=1;
-                  arr2.shift();
-                  if (i !== arr1.length-1) break;
-                  else doneWithArr1 = true;
-                }else if (arr1[i]>arr2[j]){
-                  inArr2+=1;
-                  oneArray+=1;
-                }else{
-                  inArr1+=1;
-                  oneArray+=1;
-                  if (i !== arr1.length-1) break;
-                  else{
-                    doneWithArr1 = true;
-                    inArr2+=1;
-                    oneArray+=1;
-                  }
-                }
-              }
-            console.log('After(in j):','bothArrays:',bothArrays,'oneArray:',oneArray,'inArr1:',inArr1,'inArr2:',inArr2);
-            }
-          console.log('After(in i): ','bothArrays:',bothArrays,'oneArray:',oneArray,'inArr1:',inArr1,'inArr2:',inArr2);
-          }
-            return [bothArrays, oneArray, inArr1, inArr2];
-        }
-        arr1 = [1, 2 ,3,4, 5 ,6 ,7 ,8 ,9];
-        arr2 = [2, 4, 6, 8, 10, 12, 14];			//[4, 8, 5, 3]
-        process2Arrays(arr1,arr2);
+                                //Lets loop with the other array:
+                                for(let i=0; i<arr2.length; i++){
+                                    //Is the element in arr1?
+                                    if(arr1.indexOf(arr2[i])<0) {
+                                        oneArray+=1;
+                                        inArr2+=1;
+                                    }
+                                }
+                            }
+                            return [bothArrays, oneArray, inArr1, inArr2];
+                        }
+                        arr1 = [0, 35, 17, 6, 7, 10, 11, 46, 47, 16, 49, 20, 14, 23, 25, 26, 29]; 
+                        arr2 = [0, 6, 17, 42, 43, 47, 16, 49, 50, 21, 28, 30];
+                        //result: [6, 17, 11, 6]);
+                        process2Arrays(arr1,arr2);
