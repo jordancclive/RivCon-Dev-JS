5c7d72287d4caa3a99923a06451fb0375de28389
Vincent Rios <vince@riversconsulting.com>
2016-10-07 15:04:01 -0400

    Update Sort-Selection.js

--- Sort-Selection.js
+++ Sort-Selection.js
@@ -1,9 +1,9 @@
-// Selection sort
+//Selection Sort  - O(n^2)
 
 function selectionSort(arr){
     for(let i=0; i<arr.length-1; i++){
         for(let j=i+1; j<arr.length; j++){
-            if(arr[i]>arr[j]{
+            if(arr[i]>arr[j]){
                 let temp = arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp;
@@ -13,4 +13,4 @@ function selectionSort(arr){
     return arr;
 }
 
-selectionSort([4,2,3,1);
+selectionSort([4,2,3,1]);
