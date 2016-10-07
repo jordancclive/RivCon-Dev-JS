a50b5c621be2a6a709229c60db9f29da870584bc
Vincent Rios <vince@riversconsulting.com>
2016-10-07 15:01:44 -0400

    Create Sort-Selection.js

---
+++ Sort-Selection.js
@@ -0,0 +1,16 @@
+// Selection sort
+
+function selectionSort(arr){
+    for(let i=0; i<arr.length-1; i++){
+        for(let j=i+1; j<arr.length; j++){
+            if(arr[i]>arr[j]{
+                let temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    return arr;
+}
+
+selectionSort([4,2,3,1);
