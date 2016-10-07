693a525930de116a325e22b5babc1ce437a24a68
Vincent Rios <vince@riversconsulting.com>
2016-10-07 14:32:05 -0400

    Create BigO.js

---
+++ BigO.js
@@ -0,0 +1,33 @@
+/* Big O Notation
+
+    We are looking at how efficient our program is...How the efficiency degrades as the amount of input increases.
+    
+    O(1) = code will always take the same amount of time to complete, 
+            regardless of how big the input is.
+    
+    O(N) = Exercution time will grow linearly to the size of the input.
+    
+                  For loops are a good example of this...
+                  forEach, map, reduce, filter, indexOf()
+                  searching algorithms.
+                  
+     O(N squared) = execution time will grow quadratically relative to the size of the input.
+    
+                  nested loops are an example of this.
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
