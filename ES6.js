0d5aecb013e25d1b5a669541a4f63d7b0317950d
Vincent Rios <vince@riversconsulting.com>
2016-10-09 12:27:19 -0400

    Create ES6.js

---
+++ ES6.js
@@ -0,0 +1,14 @@
+/* ES6 review
+              ECMAScript 6 = 6th release of the ECMAScript language specification.
+
+------------------------------
+The Ternary Operator:
+*/
+              var someVariable = [condition] ? [if true] : [if false]
+
+
+
+/*
+
+
+*/
