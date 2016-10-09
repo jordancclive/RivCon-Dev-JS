30b659b026d6867a71dc9980cd0721c2328ecd6a
Vincent Rios <vince@riversconsulting.com>
2016-10-09 12:42:11 -0400

    Create TernaryOperator.js

---
+++ TernaryOperator.js
@@ -0,0 +1,21 @@
+
+The Ternary Operator:
+*/
+              var someVariable = [condition] ? [if true] : [if false]
+              
+              //some examples:
+              
+              var simpleQuestion = 2>1 ? 'yes' : 'no';
+              console.log(simpleQuestion)             //result: yes
+              
+              //-----------------------------
+              
+              var dayOfMonth = 21;
+              var takesOutTrash = isEven(dayOfMonth) ? 'Vin' : 'Karen';
+              console.log(takesOutTrash);
+
+              function isEven(dayOfMonth){ return dayOfMonth % 2 ===0;}
+
+              //-----------------------------
+
+/*------------------------------
