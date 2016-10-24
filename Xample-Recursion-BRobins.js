f002709b7fa8eda2700c2157ed768c3453a38ce3
Vincent R <vince@riversconsulting.com>
2016-10-24 14:11:26 -0400

    Update Xample-Recursion-BRobins.js

--- Xample-Recursion-BRobins.js
+++ Xample-Recursion-BRobins.js
@@ -5,7 +5,9 @@
 //Players sit in a circle and hold out 1-5 fingers and begin counting them.
 //The player holding the 31st finger wins.
 
-//Complete this function. You should assign a random # of 1-5 to each player and somehow associate it with the name. Starting with Ayanna, determine who is holding up the 31'st finger and return their name
+//	Complete this function. You should assign a random # of 1-5 to each player 
+//	and somehow associate it with the name. Starting with Ayanna, 
+//	determine who is holding up the 31'st finger and return their name
 
 function  genRandom (){ 
 	let num = Math.round(Math.random()*4)+1;
@@ -20,33 +22,7 @@ var baskinRobbins31 = function (count) {
 		fingers = genRandom ();
 		count += fingers;
 		if(count>=31) return players[i];
-		else if (i === players.length-1) result = baskinRobbins31(count);
-	}
-	return result;
-};
-
-//Tests
-console.log(baskinRobbins31() + ' must drink');
-console.log(baskinRobbins31() + ' must drink');
-console.log(baskinRobbins31() + ' must drink');
-
-//--------------------------------------------------------
-// or
-
-function  genRandom (){ 
-	let num = Math.round(Math.random()*4)+1;
-	return num;
-}
-
-var baskinRobbins31 = function (count) {
-	var players = ['Ayanna', 'Ben', 'Ceren', 'Dan', 'Eric'];
-	let result = '';
-	if (typeof count !== "number") count = 0;
-	for(let i=0; i<players.length; i++){
-		fingers = genRandom ();
-		count += fingers;
-		if(count>=31) return players[i];
-		else if (i === players.length-1) return = baskinRobbins31(count);
+		else if (i === players.length-1) return baskinRobbins31(count);
 		//This previous statement is the key to recursion.
 		// Most times you are calculating something within the recursive calls and
 		// need to pass some result back up the call stack or the value will not 
