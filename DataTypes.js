8d24ab9d215baaeddc19ff2ddf34b76dda69c5e1
Vincent R <vince@riversconsulting.com>
2016-10-27 12:35:44 -0400

    Update DataTypes.js

--- DataTypes.js
+++ DataTypes.js
@@ -4,33 +4,50 @@ Null:
 
   You have defined a variable and you know what Data Type it is, but have not given it a value. 
   It is a placeholder for a variable's value.  A variable that has no value yet.
+  
 
 Undefined:
 
   You have no idea what the Data Type is for this variable yet.  The variable has been declared, but it is an unused variable.
+  
 
 String:
 
   text, always wrapped with quotes.
+  
 
 Numbers:
 
   Are numbers with all the same characteristics of numbers in the real world.  (You can perform operations on them).
   
   64 bits, 1 bit is +/-, a few store the decimal point.  Fractional digital numbers are approximate.
+  
 
 Boolean:
 
   A value of True or False.  ....takes the least amount of memory. (1 bit  --> 0 or 1).
+  
 
 Function:
 
   Chunk of reusable code.
+  
 
 Object:
 
   A specific type of Object is an Array.
+  
+      newObj = {property(key): value...};
+      
+      newArr = [1,2,3];
+      
 
+Date:
+
+
+Regular Expression:
+
+      / .* /g,
 
 
 
