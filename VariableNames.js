719c0cbb997427246881a49e255b67de42d1e0ff
Vincent R <vince@riversconsulting.com>
2016-10-28 11:34:15 -0400

    Create VariableNames.js

---
+++ VariableNames.js
@@ -0,0 +1,13 @@
+/*
+
+Variable Names catch and hold values.
+
+The name must:
+
+        - start with a character.
+        - can contain numbers in the name.
+        - can only have the following special characters: '$' or '_'. 
+        
+If the variable is empty, then 'undefined' is the value.
+
+*/
