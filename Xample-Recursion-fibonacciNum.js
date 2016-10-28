c99ecd3166d99daa1aa3289a2358842f0a19c2a5
Vincent R <vince@riversconsulting.com>
2016-10-28 11:08:34 -0400

    Update Xample-Recursion-fibonacciNum.js

--- Xample-Recursion-fibonacciNum.js
+++ Xample-Recursion-fibonacciNum.js
@@ -62,9 +62,7 @@ console.log(fib(11)); // ===> 89);
 		//recursive case
 */
 
-var fib = function(n) { //5
-	// if(n===1) return 1;
-	// if(n===0) return 0;
+var fib = function(n) { 
 	if(n <= 1) return n;
 	return fib(n-1) + fib(n-2);    //simple solution
 	
