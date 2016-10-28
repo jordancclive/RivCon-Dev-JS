84b8b7e783c8eb9af68279c8d24d590a8c2f3cac
Vincent R <vince@riversconsulting.com>
2016-10-28 13:34:38 -0400

    Update Project-SqStringMoves_1.js

--- Project-SqStringMoves_1.js
+++ Project-SqStringMoves_1.js
@@ -83,3 +83,22 @@ s = "hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu";
 console.log(oper(vertMirror, s));
 console.log('\n');
 console.log(oper(horMirror, s));
+
+
+//----------------------------------------------
+
+// More solutions that I did not think about when I was doing the problem:
+
+function vertMirror(str) {
+    return str.split('\n')
+      .map(line => line.split('').reverse().join(''))		//reversed the element
+      .join('\n')
+}
+function horMirror(str) {
+    return str.split('\n')
+      .reverse()						//reversed the array of elements.
+      .join('\n')
+}
+function oper(fct, s) {
+    return fct(s)
+}
