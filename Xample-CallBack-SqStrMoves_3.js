68d5796351c6f1ba216b4d28e400247a1589c99e
Vincent R <vince@riversconsulting.com>
2016-10-30 16:50:44 -0400

    Update and rename Xample-CallBack-SqStrMoves_3.js to Xample-CallBack-map-SqStrMoves_3.js

--- Xample-CallBack-SqStrMoves_3.js
+++
@@ -1,1 +0,0 @@
-Project-SqStrMoves_1.js
---
+++ Xample-CallBack-map-SqStrMoves_3.js
@@ -0,0 +1,8 @@
+/*
+Go to the following links:
+            Project-SqStrCompose.js
+            Project-SqStrMoves_1.js
+            Project-SqStrMoves_2.js
+            Project-SqStrMoves_3.js
+...for examples of Arrow Notation + The Call Back: .map
+*/
