1640685adfdf1d78a91d8690eb732e45d0437978
Vincent R <vince@riversconsulting.com>
2016-10-30 17:04:01 -0400

    Update and rename Xample-RegEx-SqStrMoves_2.js to Xample-RegExpressions-SqStr.js

--- Xample-RegEx-SqStrMoves_2.js
+++
@@ -1,5 +0,0 @@
-/*
-
-go to   Project-SqStrMoves_2.js   for an example of a regular expression.
-
-*/
---
+++ Xample-RegExpressions-SqStr.js
@@ -0,0 +1,10 @@
+/*
+
+go to
+
+      Project-SqStrMoves_2.js   
+      Project-SqStrMoves_3.js
+
+for an example of a regular expression.
+
+*/
