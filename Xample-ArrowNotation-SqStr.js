6cb44796db27dcb68f40973d1dab307131bb455f
Vincent R <vince@riversconsulting.com>
2016-10-31 15:15:49 -0400

    Update Xample-ArrowNotation-SqStr.js

--- Xample-ArrowNotation-SqStr.js
+++ Xample-ArrowNotation-SqStr.js
@@ -6,6 +6,7 @@ Go to the following links:
             Project-SqStrMoves_1.js
             Project-SqStrMoves_2.js
             Project-SqStrMoves_3.js
+            Project-SqStrMoves_4.js
 
 ...for examples of Arrow Notation + The Call Back: .map
 
