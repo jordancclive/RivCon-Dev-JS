992dc2c282b8a55f6491b17ed15301c5a2e8f425
Vincent R <vince@riversconsulting.com>
2016-11-02 11:38:52 -0400

    Update and rename Xample-RegExpressions-SqStr.js to Xample-RegExpressions.js

--- Xample-RegExpressions.js
+++ Xample-RegExpressions.js
@@ -5,6 +5,7 @@ go to
       Project-SqStrMoves_2.js   
       Project-SqStrMoves_3.js
       Project-WordLinesMarkovChain.js
+      Xample-Drake_Generator.js
 
 for an example of a regular expression.
 
