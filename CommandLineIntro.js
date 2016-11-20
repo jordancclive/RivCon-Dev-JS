d9d5208424304428934aeac70e4e0738ea11a5cc
Vince R <vince@riversconsulting.com>
2016-11-19 22:26:40 -0500

    Update and rename CommandLine.js to CommandLineIntro.js

---
+++ CommandLineIntro.js
@@ -0,0 +1,49 @@
+// The Command Line:  AKA: Console or Terminal.
+
+/*
+
+What is a Graphical User Interface (GUI)?
+
+        Utilizes graphics (like icons) to interact with the O/S and perform tasks.  ...like opening & closing programs.
+        
+        Certain tasks in a GUI are cumbersome....so you might use a CLI instead...
+
+
+What is a Command Line Interface (CLI)?
+
+        There are no graphics or icons.  
+        
+        It is a text based interface that allows one to interact with the O/S.  
+        You would not use a mouse.  You would type commands on the keyboard.
+        
+        Advantages:
+        
+        - Speed.
+        - System Resources.  You use significantly less resources using a CLI.
+        - Diversity.  After learning a CLI, it usually stays consistant (won't change much)
+                      other than possibly adding new commands.
+        - Flexible & Powerful.  It is possible to run scripts and/or run mini programs that perform tasks.
+                      You can also write a one line command that does something in a global kind of way.
+                      ....as an example:  copying a whole folder's worth of files from one place to another.
+        
+        
+        
+Note:  We will be discussing using the command lne within a UNIX O/S environment.
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
