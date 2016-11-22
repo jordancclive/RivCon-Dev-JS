cd0f27ba252bdf006e761b122eb9a9192d48ad4d
Vince R <vince@riversconsulting.com>
2016-11-22 16:25:27 -0500

    Update and rename CommandLineIntro.js to CommandLine-Intro.js

---
+++ CommandLine-Intro.js
@@ -0,0 +1,164 @@
+/* The Command Line:  AKA: Console or Terminal.
+
+
+What is a Graphical User Interface (GUI)?
+
+        Utilizes graphics (like icons) to interact with the O/S and perform tasks.  ...like opening & closing programs.
+        
+        Certain tasks in a GUI are cumbersome....so you might use a CLI instead...
+
+
+What is a Command Line Interface (CLI)?
+
+        There are no graphics or icons.  
+        
+        It is a text based interface that allows one to interact with the O/S.  
+        You would not use a mouse.  You would type commands on the keyboard.
+        
+        Advantages:
+        
+        - Speed.
+        - System Resources.  You use significantly less resources using a CLI.
+        - Diversity.  After learning a CLI, it usually stays consistant (won't change much)
+                      other than possibly adding new commands.
+        - Flexible & Powerful.  It is possible to run scripts and/or run mini programs that perform tasks.
+                      You can also write a one line command that does something in a global kind of way.
+                      ....as an example:  copying a whole folder's worth of files from one place to another.
+        
+        
+        
+Note:  We will be discussing using the command lne within a UNIX O/S environment.
+
+
+
+===============================================================      
+                 Anatomy of the Command line
+===============================================================   
+
+         rivermans-MacBook-Pro $ rm -f foundations.txt
+        |-----------------------|
+        
+        |-->    This is the Prompt.  ...which is supplied by the command line.
+                Gives you information about your working directory or git branch that you are working on.
+                It always ends with a '$'.
+        ===============================================================   
+
+         rivermans-MacBook-Pro $ rm -f foundations.txt
+                                |--|
+         
+         Next is the Command.  <---|    
+         A Command is an instruction that the user supplies that performs an action on the upcoming argument.
+         In this case, 'rm' stands for remove.
+          ===============================================================   
+
+         rivermans-MacBook-Pro $ rm -f foundations.txt
+                                   |--|           
+
+         Next is the Option / Flag.<--| 
+         Options are optional.  An Option always starts with a dash and is usually a single character.
+            ===============================================================   
+
+         rivermans-MacBook-Pro $ rm -f foundations.txt
+                                      |---------------|  
+                                      
+         Next is the Argument.                     <--| 
+
+        A Command is always followed by an argument.
+        
+
+
+==========================================================================
+        command spacebar = brings up spotlight
+
+        In spotlight type iterm.....  will get the iterm command line interface.  
+        ....the black screen is called a "shell'
+==========================================================================
+
+        A Shell is a program that interprets commands and acts as an intermediary 
+        between the user and the inner workings of the operating system (O/S).
+        
+==========================================================================
+
+  The echo command prints its argument to the screen.
+  
+              design-admins-Mac-Pro$ echo foundations
+              foundations                                             <---It displays the argument
+              
+              design-admins-Mac$ echo 'Grace Hopper'                  <---It displays the argument
+              Grace Hopper
+              
+              design-admins-Mac$ echo "Vin                            <---if you do not close the quotes
+              >                                                           the system gets stuck
+                                                                           use ctrl c to stop the 
+                                                                           execution the current cmd
+                                                                           or process.
+              design-admins-Mac$                                      <---which will bring you back to here.
+              
+              
+              NOTE: You could have put in the ending quote on that line with a carrot and the
+                    terminal would have finished the command from the previous line as follows:
+                    
+              design-admins-Mac$ echo "vin
+              > "
+              vin
+              
+==========================================================================
+
+                Cursor Navigation
+                
+ ==========================================================================               
+
+                CTRL + a : Move to begining of the command line
+                CTRL + e : Move to end
+                
+                Use Up and Down arrow keys to toggle through previous commands
+                
+                CTRL + u : Clears current line
+                CTRL + l : Clears the terminal window (does not clear history)
+                
+  ==========================================================================
+
+                Commands
+                
+ ==========================================================================                    
+
+                ls : List command (lists the directories in from the current directory)
+                
+                ls + directory : Lists the directories from the argument
+                
+                pwd : print working directory
+                
+                File Path : A files location in the Filesystem
+                
+                Directory : A "folder"
+                
+                Standard Output : The output in the Command Line after a command
+                
+                man + command : Manual, displays the manual for the current command
+                
+                ls -a : Displays all of the "hidden" files in a directory
+                
+                q : Another "get out of trouble" command (try q when CTRL + C doesn't work
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
