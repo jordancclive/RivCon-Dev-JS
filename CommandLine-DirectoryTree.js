c3b5c7e1a049c0ac71cd9c6412e30b6ce40a20e8
Vince R <vince@riversconsulting.com>
2016-11-23 17:52:13 -0500

    Update CommandLine-DirectoryTree.js

--- CommandLine-DirectoryTree.js
+++ CommandLine-DirectoryTree.js
@@ -1,13 +1,62 @@
 /* Directories & the Directory Tree
 
-==========================================================================
+        ==========================================================================
+
+              Directory:  Is a Folder on your computer.  
+                  
+                        "A grouping of files"
+          
+        ==========================================================================        
+
+              Filesystem: is the tree like structure starting at the root directory.
+                          It has multiple sub-directories with files inside of it.
+                    
+              The Command Line: is always somewhere in the filesystem.
+              
+              Note:  the command: "pwd" returns the directory that you are in.
+
+        ==========================================================================        
+
+              File Path:  Contains all the directories needed in sequential order 
+                          to find a specific file.
+                          
+              Each directory in the file path is separated by a forward slash.
+
+        ==========================================================================        
+
+              There are 2 types of paths:
+
+                    Absolute Path:
+                    
+                    
+                    Relative Path:
+
+
+        ==========================================================================        
+
+                          Directory Tree Like Structure
+
+                                                 /                                         <---Root Directory is the "/"
+              -------------------------------------------------------------------------
+                 |                     |                     |                      |
+                bin                   etc                   usr                   Users    <---1st Level Directories
+                 |                                                                  |
+          ---------------------                                         --------------------------
+          |       |        |                                               |        |         |
+          ls     echo     pwd                                             Vin     Scott     Karen
+                                                                           |
+                                                                       -----------
+                                                                          home
+                                                                           |
+                                                  ------------------------------------------------------
+                                                        |             |             |             |
+                                                    Documents   Applications    Fullstack   GraceHopper
 
 
 
 
 
-
-
+        ==========================================================================        
 
 
 
