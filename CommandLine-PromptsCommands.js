a146cd1ff262c7013c270be31e2dcfb32cd38da2
Vince R <vince@riversconsulting.com>
2016-11-22 22:38:54 -0500

    Update CommandLine-PromptsCommands.js

--- CommandLine-PromptsCommands.js
+++ CommandLine-PromptsCommands.js
@@ -1,43 +1,6 @@
 /*Prompts & Commands
 
 ==========================================================================
-        command spacebar = brings up spotlight
-
-        In spotlight tpye iterm.....  will get the iterm command line interface.  
-        ....the black screen is called a "shell'
-==========================================================================
-
-        A Shell is a program that interprets commands and acts as an intermediary 
-        between the user and the inner workings of the operating system (O/S).
-        
-==========================================================================
-
-  The echo command prints its argument to the screen.
-  
-              design-admins-Mac-Pro$ echo foundations
-              foundations                                             <---It displays the argument
-              
-              design-admins-Mac$ echo 'Grace Hopper'                  <---It displays the argument
-              Grace Hopper
-              
-              design-admins-Mac$ echo "Vin                            <---if you do not close the quotes
-              >                                                           the system gets stuck
-                                                                           use ctrl c to stop the 
-                                                                           execution the current cmd
-                                                                           or process.
-              design-admins-Mac$                                      <---which will bring you back to here.
-              
-              
-              NOTE: You could have put in the ending quote on that line with a carrot and the
-                    terminal would have finished the command from the previous line as follows:
-                    
-              design-admins-Mac$ echo "vin
-              > "
-              vin
-        
-
-
-
 
 
 
