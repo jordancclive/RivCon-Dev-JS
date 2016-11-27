f58ac47727474f41ebab6f85219cc0b3d0091f33
Vince R <vince@riversconsulting.com>
2016-11-27 15:54:07 -0500

    Update CommandLine-DirectoryTree.js

--- CommandLine-DirectoryTree.js
+++ CommandLine-DirectoryTree.js
@@ -21,15 +21,49 @@
                           to find a specific file.
                           
               Each directory in the file path is separated by a forward slash.
+              
+              as an example:  /Users/Scott/Foundations
+                              ^   ^    ^       ^      
+                              |   |    |       |
+             The root directory   |    |       |
+             is in front of this  |    |       You will find your file here.                   
+             directory            |    Then go to this directory                         
+                                  Go to this directory        
 
         ==========================================================================        
 
-              There are 2 types of paths:
+              There are 2 types of file paths:
+              
+              Definition: File Paths are "directions" or "instructions" to a specific location in the filesystem.
 
-                    Absolute Path:
+
+                    Absolute Path:      contains every directory including the root to access
+                                        a specific location in the filesystem
+  
+                                                as an example:  /Users/Vin/Home/GraceHoper
+                                                                ^  ^    ^   ^      ^
+                                                                |  |    |   |      |
+                                               The root directory  |    |   |      You will find your file here. 
+                                               is in front of this |    |   Then go to this directory                   
+                                               directory           |    Then go to this directory                         
+                                                                   Go to this directory   
+                                        
+                                        NOTE:  See the Directory Tree (below)
                     
                     
-                    Relative Path:
+                    Relative Path:      This path relates to the directory that we are currently in.
+                    
+                                        Let's use the command "pwd" to get the current working directory.
+                                        and assume we got ---> /Users/Vin/Home/GraceHoper
+                                        
+                                        Let's also assume that we want to go to Karen's directory. (see below)
+                                        In words, we would have to go up 3 directories to "Home" and then
+                                        down 1 directory to "Karen".
+                                        
+                                        ...to go up and access the directory above the current one use ".."
+                                        ...remember the "/" as a separator for multiple moves.
+                    
+                                        ...so to get to & access a file in "Karen" --->  ../../../Karen
 
 
         ==========================================================================        
@@ -40,12 +74,12 @@
               -------------------------------------------------------------------------
                  |                     |                     |                      |
                 bin                   etc                   usr                   Users    <---1st Level Directories
-                 |                                                                  |
-          ---------------------                                         --------------------------
-          |       |        |                                               |        |         |
-          ls     echo     pwd                                             Vin     Scott     Karen
+                 |                                           |                      |
+          ---------------------             The unix system  |         --------------------------
+          |       |        |                   resources                   |        |         |
+          ls     echo     pwd                 dir is here                 Vin     Scott     Karen
                                                                            |
-                                                                       -----------
+     (This is the binaries directory)                                 -----------
                                                                           home
                                                                            |
                                                   ------------------------------------------------------
@@ -58,7 +92,18 @@
 
         ==========================================================================        
 
-
+                        Important Directories
+                        
+                        
+        *       The /bin or /sbin - both directories contain basic commands & programs needed to achieve a minimal working 
+                environment upon booting your machine.
+                
+                They mostly contain system commands.  "sbin" also contains administrative utilities.
+                Most things are compiled programs.
+                
+                like: ---->  echo, pwd and the ls (list) commands.  They are all located in the bin directory.
+                
+                
 
 
 
