f0cdcdb8c29643263cfbea81dfe6dc827009cb7c
Vince R <vince@riversconsulting.com>
2016-11-28 15:55:03 -0500

    Update CommandLine-TheShell.js

--- CommandLine-TheShell.js
+++ CommandLine-TheShell.js
@@ -1,6 +1,26 @@
 /*
+---------------------------------------------------------------------------------------------
 
+                          The Shell - an overview
+            
+---------------------------------------------------------------------------------------------
 
+      I am using iterm2 to type/submit commands and receive feedback from the O/S.
 
+      The Shell is the program that prcesses (interprets) each command 
+      in a language that the O/S understands and returns the output for iterm to display.
+
+      ...A Shell is a Command Line Interpreter and iTerm is a Command Line Interface.
+    
+---------------------------------------------------------------------------------------------
+  
+  Process Flow:
+  
+      |-->   Command line Interface (iTerm)   --->  Command Line Interpreter (Shell) ---> O/S   --->|
+      |<---------------                          <--------                              <-----------|
+      
+ ---------------------------------------------------------------------------------------------     
+  
+      The native (default) Shell for the Mac is called: Bourne Again Shell aka: BASH
 
 */
