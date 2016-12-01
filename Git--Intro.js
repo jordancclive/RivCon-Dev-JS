7182441aadb89f7fab5c63fd50942110b8b017a8
Vince R <rivermanvr@users.noreply.github.com>
2016-11-30 22:24:01 -0500

    Update Git--Intro.js

--- Git--Intro.js
+++ Git--Intro.js
@@ -88,8 +88,26 @@ The Three States of Git:
 
 ------------------------------------------------------------------------------------------
 
-First Time Setup - Getting started:
+                  First Time Setup - Getting started:
 
+------------------------------------------------------------------------------------------
+
+Tool:   git config
+
+        Git comes with a tool called git config that lets you get and set configuration variables 
+        that control all aspects of how Git looks and operates. 
+        These variables can be stored in three different places:
+
+        /etc/gitconfig file: 
+        
+                  Contains values for every user on the system and all their repositories. 
+                  If you pass the option --system to git config, it reads and writes from this file specifically.
+
+        ~/.gitconfig or ~/.config/git/config file: Specific to your user. You can make Git read and write to this file specifically by passing the --global option.
+
+        config file in the Git directory (that is, .git/config) of whatever repository you’re currently using: Specific to that single repository.
+
+        Each level overrides values in the previous level, so values in .git/config trump those in /etc/gitconfig.
 
 
 
