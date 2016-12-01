8279e0448538d1ae8b62eb02f68421f7ad26eceb
Vince R <rivermanvr@users.noreply.github.com>
2016-11-30 22:56:17 -0500

    Update Git--Intro.js

--- Git--Intro.js
+++ Git--Intro.js
@@ -98,16 +98,131 @@ Tool:   git config
         that control all aspects of how Git looks and operates. 
         These variables can be stored in three different places:
 
+
         /etc/gitconfig file: 
         
-                  Contains values for every user on the system and all their repositories. 
-                  If you pass the option --system to git config, it reads and writes from this file specifically.
+                Contains values for every user on the system and all their repositories. 
+                If you pass the option --system to git config, it reads and writes from this file specifically.
 
-        ~/.gitconfig or ~/.config/git/config file: Specific to your user. You can make Git read and write to this file specifically by passing the --global option.
 
-        config file in the Git directory (that is, .git/config) of whatever repository you’re currently using: Specific to that single repository.
+        ~/.gitconfig or ~/.config/git/config file: 
+        
+                Specific to your user. 
+                You can make Git read and write to this file specifically by passing the --global option.
 
+
+        config file in the Git directory (that is, .git/config) of whatever repository you’re currently using: 
+        
+                Specific to that single repository.
+
+        
         Each level overrides values in the previous level, so values in .git/config trump those in /etc/gitconfig.
+
+------------------------------------------------------------------------------------------
+
+Your Identity:
+
+        The first thing you should do when you install Git is to set your user name and email address. 
+        This is important because every Git commit uses this information, 
+        and it’s immutably baked into the commits you start creating:
+
+
+        $ git config --global user.name "John Doe"
+        $ git config --global user.email johndoe@example.com
+        
+        Again, you need to do this only once if you pass the --global option, 
+        because then Git will always use that information for anything you do on that system. 
+        If you want to override this with a different name or email address for specific projects, 
+        you can run the command without the --global option when you’re in that project.
+
+        Many of the GUI tools will help you do this when you first run them.
+
+------------------------------------------------------------------------------------------
+
+Your Editor:
+
+        Now that your identity is set up, you can configure the default text editor 
+        that will be used when Git needs you to type in a message. 
+        If not configured, Git uses your system’s default editor.
+
+        If you want to use a different text editor, such as Emacs, you can do the following:
+
+
+                $ git config --global core.editor emacs
+        
+        
+        While on a Windows system, if you want to use a different text editor, such as Notepad++, you can do the following:
+
+
+                On a x86 system
+
+                $ git config --global core.editor "'C:/Program Files/Notepad++/notepad++.exe' -multiInst -nosession"
+                On a x64 system
+
+                $ git config --global core.editor "'C:/Program Files (x86)/Notepad++/notepad++.exe' -multiInst -nosession"
+
+------------------------------------------------------------------------------------------
+
+Checking Your Settings
+
+        If you want to check your settings, you can use the   --->      git config --list command 
+        to list all the settings Git can find at that point:
+
+
+                $ git config --list
+                user.name=John Doe
+                user.email=johndoe@example.com
+                color.status=auto
+                color.branch=auto
+                color.interactive=auto
+                color.diff=auto
+                ...
+        
+        
+        You may see keys more than once, because Git reads the same key from different files 
+        (/etc/gitconfig and ~/.gitconfig, for example). 
+        In this case, Git uses the last value for each unique key it sees.
+
+        You can also check what Git thinks a specific key’s value is by typing git config <key>:
+
+                $ git config user.name
+                John Doe
+                
+------------------------------------------------------------------------------------------
+
+Getting Help
+
+        If you ever need help while using Git, there are three ways 
+        to get the manual page (manpage) help for any of the Git commands:
+
+
+                $ git help <verb>
+                $ git <verb> --help
+                $ man git-<verb>
+                
+        For example, you can get the manpage help for the config command by running
+
+
+                $ git help config
+                
+                
+        These commands are nice because you can access them anywhere, even offline. 
+        If the manpages and this book aren’t enough and you need in-person help, 
+        you can try the #git or #github channel on the Freenode IRC server (irc.freenode.net). 
+        These channels are regularly filled with hundreds of people who are all very knowledgeable 
+        about Git and are often willing to help.
+                
+------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
 
 
 
