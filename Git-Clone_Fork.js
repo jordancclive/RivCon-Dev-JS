d49587bf75b8bd04253bbcda12dd577a4abf24d9
Vince R <rivermanvr@users.noreply.github.com>
2016-12-06 23:29:59 -0500

    Update Git-Clone_Fork.js

--- Git-Clone_Fork.js
+++ Git-Clone_Fork.js
@@ -33,7 +33,35 @@ On the Terminal/Console:
 
 At this point you can make some changes to the repository using the text editor "sublime" as an example.
 
-        Type the command:     subl .
+        Type the command:     subl .    <---If this worked, I would see the dirextory tree inside sublime.
+        I am now running sublime outside of the console.
+        
+        You can change the files in the working directory........stage them and commit them.
+        This will make your local repository up to date. 
+        
+        ...but if you run a (git status) at this second you will get a message:  
+        
+                On branch master
+                Your branch is ahead of "origin/master" by X number of commits.
+                (use "git push" to publish your local commits)
+                nothing to commit, working directory clean.
+                
+At this point you need to "push" the updated repository to the git.com forked repository.
+
+        Type the command:     git push origin master
+        
+        This will synchronize the 2 repositories.  Result:
+        
+                Counting objects: 3, done.
+                Delta compression using up to 4 threads.
+                Compressing oObjects: 100% (3/3), done.
+                Writing Objects: 100% (3/3), 348 bytes | 0 bytes/s, done.
+                Total 3 (delta 2), reused 0 (delta 0)
+                To https://github.com/.......... .git
+                    5325313..9e37c55  master -> master
+             
+
+                
                 
                 
                 
