5d258a1c9b9ab72d7027e7516db80bfa26b5c349
Vince R <rivermanvr@users.noreply.github.com>
2016-12-06 22:26:10 -0500

    Create Git-Clone_Fork.js

---
+++ Git-Clone_Fork.js
@@ -0,0 +1,34 @@
+/*
+---------------------------------------------------------------------
+
+                Cloning &/or Forking a Repository
+
+---------------------------------------------------------------------
+
+Start with clicking the "Fork" button in the upper right corner.
+
+        - The system will ask you where you want to put it.  --->generally select your account.
+        
+The system will create a copy of the entire repository in your account.
+
+Once you have a fork (a copy), you will want to clone it to your local computer.
+
+        - select "clone" and copy the HTTPS link listed. 
+        
+On the Terminal/Console:
+        
+        Create a general container folder ---> "Foundations" to designate Fullstack repositories.
+
+        Now clone the "fork" by typing the following command:  git clone <HTTPS link>
+
+        
+
+
+
+
+
+
+
+
+
+*/
