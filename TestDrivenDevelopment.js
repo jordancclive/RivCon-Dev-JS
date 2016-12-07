b70dc8b2586eb4da9aae5336f6b36ca49f189632
Vince R <rivermanvr@users.noreply.github.com>
2016-12-07 15:51:23 -0500

    Update TestDrivenDevelopment.js

--- TestDrivenDevelopment.js
+++ TestDrivenDevelopment.js
@@ -1,10 +1,15 @@
 /*
+---------------------------------------------------------------------
 
+                    Test Driven Development
 
+---------------------------------------------------------------------
 
+Overview:
 
-
-
+        The proper workflow (and best practice) for TDD(Test Driven Development) is to first write tests, 
+        then create the code that passes the tests.
+        
 
 
 
