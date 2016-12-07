29624e8d8e25ad583e8429cbdcedde601f937948
Vince R <rivermanvr@users.noreply.github.com>
2016-12-07 13:39:15 -0500

    Create Git-Testem_Github.js

---
+++ Git-Testem_Github.js
@@ -0,0 +1,37 @@
+/*
+-------------------------------------------------------------------------
+
+                        Testem - running specs
+                        
+                        
+                     Uploading git changes to github
+
+-------------------------------------------------------------------------
+
+To run the specs for a directory, you need to go into that directory and run the command testem.
+
+                    $ testem
+                    TEST'EM 'SCRIPTS!
+                    Open the URL below in a browser to connect.
+                    http://localhost:7357/
+
+
+
+Even though we will not be grading your TestFirst assignments, we encourage you to commit your work and push your commits up to your fork of TestFirst-Checkpoint-Prep-Part-1.
+
+                    $git add .
+                    $git commit -m 'your commit message'
+                    $git push origin master
+
+
+
+Not only is this good practice for you, but you will need to push your work up to GitHub in this manner when you take your Checkpoints so that they can be graded. Get used to using Git now before you start your Checkpoints! If you are lost, please consult the 'Exploring Git' section of this workshop, or post your questions on Discuss or the Help Desk.
+
+
+
+
+
+
+
+
+*/
