a518feec6d11aca7a85a108c25342a84b0381de3
Vince R <rivermanvr@users.noreply.github.com>
2016-12-08 11:31:55 -0500

    Create Arrays-Concepts.js

---
+++ Arrays-Concepts.js
@@ -0,0 +1,33 @@
+/*
+------------------------------------------------------------------------------
+
+                                Arrays
+                                
+                            Things to know
+
+------------------------------------------------------------------------------
+
+Arrays are specific kinds of Objects.....
+*/
+
+
+var arr = [1,2,3,4];
+undefined
+arr.karen = "karen";
+"karen"
+dir(arr);
+VM290:1 Array[4]0: 11: 22: 33: 4karen: "karen"length: 4__proto__: Array[0]
+undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
