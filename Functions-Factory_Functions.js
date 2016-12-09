71086432dcfa2b190d669bc165091baa02b3da97
Vince R <rivermanvr@users.noreply.github.com>
2016-12-08 22:59:33 -0500

    Create Functions-Factory_Functions.js

---
+++ Functions-Factory_Functions.js
@@ -0,0 +1,35 @@
+/*
+----------------------------------------------------------------------------------
+
+                          Factory Functions
+
+----------------------------------------------------------------------------------
+
+Definition:
+
+        Allows you to create reusable objects whith pre-defined properties and methods.
+        
+Example:
+*/
+                function newUser(handle){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+*/
