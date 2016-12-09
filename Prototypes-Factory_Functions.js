f2079a33ec383150c3e34134d209e1a9c64cda62
Vince R <rivermanvr@users.noreply.github.com>
2016-12-08 23:13:59 -0500

    Update and rename Functions-Factory_Functions.js to Prototypes-Factory_Functions.js

---
+++ Prototypes-Factory_Functions.js
@@ -0,0 +1,56 @@
+/*
+----------------------------------------------------------------------------------
+
+                          Factory Functions
+
+----------------------------------------------------------------------------------
+
+Definition:
+
+        Allows you to create reusable objects whith pre-defined properties and methods.
+        
+Example:
+*/
+                function newUser(handle){
+                    var twitterUser = {
+                              handle: handle,
+                              followers:  [],
+                              addFollower:  function(followerHandle){
+                                      twitterUser.followers.push(followerHandle);
+                              }
+                    }
+                    
+                    return twitterUser;
+                }
+
+//              Another way to do this is as follows:
+
+                function newUser(handle){
+                    return {
+                              handle: handle,
+                              followers:  [],
+                              addFollower:  function(followerHandle){
+                                      this.followers.push(followerHandle);    //<----The 'this' keyword.
+                              }  
+                    }
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+*/
