d627abeb6b1519feffc86f97fb7e0cc3cf254d38
Vince R <rivermanvr@users.noreply.github.com>
2016-12-09 23:07:56 -0500

    Update Prototypes-ConstructorFunctions.js

--- Prototypes-ConstructorFunctions.js
+++ Prototypes-ConstructorFunctions.js
@@ -126,6 +126,50 @@ Let's do it:
 /*
 -------------------------------------------------------------------------------
 
-ANother Example:
+Another Example:
+*/
+	function Car(name, type, year){					//<----This is our constructor function
+		this.name=name;
+		this.type=type;
+		this.year=year;
+		this.keyInserted=false;
+	}
 
+	var instanceOfCar = new Car("Corolla", "Toyota", 2005);		//<----we are building our instance here
+	
+	//Let's add some methods to the prototype.
+	
+	Car.prototype.startEngine = function(){
+	  if(this.keyInserted === false){
+	    throw "Please insert a key";				//<----Throwing an errot here if you haven't
+	  }								//	inserted your key.
+	  return "Engine Started";					//Note: errors stop the code and leave the function
+	}								//	as if you ran a return.
+	
+	Car.prototype.insertKey = function(){
+	  this.keyInserted = !this.keyInserted;
+	}
+	
+	//note: key inserted = false, so cannot yet start the car.
+	instanceOfCar.insertKey();					//<---we set the keyInserted value to true.
+	instanceOfCar.startEngine();					//<---	"Engine Started" message.
 
+	// Let's try it anyway:
+	instanceOfCar.insertKey();					//<---we set the keyInserted value back to false.
+	instanceOfCar.startEngine();					//<---tripped our error: Uncaught Please insert a key     
+
+	console.log(instanceOfCar);
+
+	Car {name: "Corolla", type: "Toyota", year: 2005, keyinserted: false}	//<----our console.log of the instance
+
+	//Examined the instance as follows:
+	Car
+		keyInserted: false
+		name: "Corolla"
+		type: "Toyota"
+		year: 2005
+		__proto__: Object
+			constructor: function Car(name, type, year)	//<---shows that this is an instance of Car constructor
+			insertKey: function()				//<---our prototype methods are here
+			startEngine: function()
+			__proto__: Object
