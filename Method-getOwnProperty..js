e85a31e285c7325538c4bb34f2e4d40b0b9f38e9
Vince R <rivermanvr@users.noreply.github.com>
2016-12-11 16:32:01 -0500

    Update Method-getOwnProperty..js

--- Method-getOwnProperty..js
+++ Method-getOwnProperty..js
@@ -11,12 +11,18 @@
 The above are powerful methods.....
 */
               function hello(str){
+                    return str;
+              }
+              hello.myProperty = 10;
+
+              typeof hello                                //<---- function  (because this object is callable
+                                                          //      it is designated as a "function" (a type of Object)
+              Object.getOwnPropertyNames(hello);          //<----The properties that exist for a function returns in an array.
               
-
-
-
-              Object.getOwnPropertyNames(hello);
-
+              //result:   
+              //["length", "name", "arguments", "caller", "prototype", "myProperty"]
+                                                                            ^
+              //                                                           Here is the property that we created (above)
 
 
 
