b55b484b1be5aed2df924856052fc5dca6b9a2bc
Vince R <rivermanvr@users.noreply.github.com>
2016-12-10 22:15:10 -0500

    Update Loops-For_In-hasOwnProperty.js

--- Loops-For_In-hasOwnProperty.js
+++ Loops-For_In-hasOwnProperty.js
@@ -65,13 +65,116 @@ For_In loops:
 	
 ------------------------------------------------------------------
 
+An Example:
+*/
+	function Car(name, type, year){					//<----This is our constructor function
+		this.name=name;
+		this.type=type;
+		this.year=year;
+		this.keyInserted=false;
+	}
+
+	var instanceOfCar = new Car("Corolla", "Toyota", 2005);		//<----we are building our instance here
+	
+	//Let's add some methods to the prototype.
+	
+	Car.prototype.startEngine = function(){
+	  if(this.keyInserted === false){
+	    throw "Please insert a key";				//<----Throwing an errot here if you haven't
+	  }								//	inserted your key.
+	  return "Engine Started";					//Note: errors stop the code and leave the function
+	}								//	as if you ran a return.
+	
+	Car.prototype.insertKey = function(){
+	  this.keyInserted = !this.keyInserted;
+	}
+	
+	//note: key inserted = false, so cannot yet start the car.
+	instanceOfCar.insertKey();					//<---we set the keyInserted value to true.
+	instanceOfCar.startEngine();					//<---	"Engine Started" message.
+
+	// Let's try it anyway:
+	instanceOfCar.insertKey();					//<---we set the keyInserted value back to false.
+	instanceOfCar.startEngine();					//<---tripped our error: Uncaught Please insert a key     
+
+	console.log(instanceOfCar);
+
+	Car {name: "Corolla", type: "Toyota", year: 2005, keyinserted: false}	//<----our console.log of the instance
+
+	//Examined the instance as follows:
+	Car
+		keyInserted: false
+		name: "Corolla"
+		type: "Toyota"
+		year: 2005
+		__proto__: Object
+			constructor: function Car(name, type, year)	//<---shows that this is an instance of Car constructor
+			insertKey: function()				//<---our prototype methods are here
+			startEngine: function()
+			__proto__: Object
+			
+//------------------------------------------------------------------
+	
+	for(var key in instanceOfCar) {
+		console.log(key, instanceOfCar[key]);		//<---	we are doing a for_In Loop
+	}							//  *** take a look at the results below ***
+/*
+results of For_In Loop:
+
+name Corolla
+type Toyota
+year 2005
+keyInserted false							*** IMPORTANT ***
+
+startEngine (){					<---	The for_In Loop looked at the methods that are on the 
+	if(this.keyInserted === false){			internal prototype in addition to the objects keys.
+	   throw "Please insert a key";			
+	}						For_In access EVERY PROPERTY even in the prototype chain
+	return "Engine Started";			that is innumerable (aka: can be counted, countable). 
+}							
+							Innumerability is a characteristic of a property itself.
+insertKey (){						In other words, you can set Innumerability to true or false.
+	this.keyInserted = !this.keyInserted;		
+}							We created these properties and innumerability
+							is set to true by default.
+							
+							The built-in properties on the prototype have their 
+							innumerability set to false by default.
+
+------------------------------------------------------------------
+
+If we do not want to loop over your user created methods, 		<--- IMPORTANT 
+there are a few ways of handing this:
+
+	by using the hasOwnPropeerty() method that is built in to all objects, as follows:
+*/
+	for(var key in instanceOfCar) {					//<---	we are doing a for_In Loop
+		if(instanceOfCar.hasOwnProperty(key)){
+		   	console.log(key, instanceOfCar[key]);		//<---	Now we are using the 
+		}							//	.hasOwnProperty() method
+	}
+/*
+		results of For_In Loop using the .hasOwnProperty() method:
+
+		name Corolla						//<---	MUCH BETTER!!!
+		type Toyota
+		year 2005
+		keyInserted false
+
+------------------------------------------------------------------
+
+An easier way of looping through an object and 
+ensuring you have only the direct & innumrerable keys is to use:
+
+	let keyArr = Object.keys(instanceOfCar);		<--- 	This will give you an array result
+									with all the keys of the object as values.			
+	Object.keys(instanceOfCar);
+	["name", "type", "year", "keyInserted"]			<---	There you go.
+									Now you can use a for loop
+	keyArr;								or for each...
+	["name", "type", "year", "keyInserted"]
 
 
 
 
-
-
-
-
-
-
+*/
