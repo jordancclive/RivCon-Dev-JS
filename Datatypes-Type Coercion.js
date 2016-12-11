f27bf5482ce7af2f6a5c0802b7948a34c8788060
Vince R <rivermanvr@users.noreply.github.com>
2016-12-10 23:23:35 -0500

    Create Datatypes-Type Coercion.js

---
+++ Datatypes-Type Coercion.js
@@ -0,0 +1,38 @@
+/*
+-----------------------------------------------------------------------
+
+                             Type Coercion
+
+-----------------------------------------------------------------------
+
+JavaScript is a loosely typed language.  
+Meaning that variables are declared without a type.
+
+Datatypes are derived internally by JavaScript.  
+...and can change internally.
+
+Which brings us to Type Coercion.  
+javascript will try to make sense out of what you are doing and try to 
+come up with a result....as follows:
+
+        4 + 5 = 9
+        "a" + "b" = "ab"
+        'a' + 5 = "a5"            <----
+        'a' + [1,2,3] = 'a1,2,3'  <----
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
