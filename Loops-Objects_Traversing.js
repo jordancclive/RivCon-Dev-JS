0dcc7b10d72d57435ecbd9c0e127c889080c5c14
Vince R <rivermanvr@users.noreply.github.com>
2016-12-11 00:37:20 -0500

    Update Loops-Objects_Traversing.js

--- Loops-Objects_Traversing.js
+++ Loops-Objects_Traversing.js
@@ -5,12 +5,31 @@
 
 ------------------------------------------------------------------------
 
+There are two main ways that you can loop through an object: 
+a for-in loop, and using the Object.keys method.
 
+------------------------------------------------------------------------
 
+Let’s start with the for in loop.
 
+        The for in loop has different syntax than a regular for loop, 
+        as you’re not defining a counter. 
+        Instead, a for-in loop will loop through all the enumerable properties of an object 
+        and that object’s internal prototype chain.
+        
+                var myDog = {name: 'Duke', breed: 'Labrador', color: 'Chocolate'};
+                
+                for(var key in myDog) {
+                    console.log(key);
+                }
 
+                result:
+                
+                "name"
+                "breed"
+                "color"
 
-
+------------------------------------------------------------------------
 
 
 
