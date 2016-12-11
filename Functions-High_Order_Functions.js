fb95ff313810ace869e5bb3365e502fca859f260
Vince R <rivermanvr@users.noreply.github.com>
2016-12-11 15:37:18 -0500

    Create Functions-High_Order_Functions.js

---
+++ Functions-High_Order_Functions.js
@@ -0,0 +1,38 @@
+/*
+-----------------------------------------------------------------------------
+
+                              High_Order_Functions
+
+-----------------------------------------------------------------------------
+
+Definition:
+
+        - A function that can take another function as its argument.
+        
+        or
+        
+        - returns a function.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
