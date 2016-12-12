342d8be92e3a04c670cddc2413b1953fcc97eaf8
Vince R <rivermanvr@users.noreply.github.com>
2016-12-11 22:41:23 -0500

    Update and rename Scope_Hoisting.js to Scope.js

--- Scope.js
+++ Scope.js
@@ -1,8 +1,20 @@
-//Scope
+/*
+-----------------------------------------------------------------------------  
+                              A word about Scope
+                                
+-----------------------------------------------------------------------------  
 
-/* Scope is the set of variables you can access at any given time.
+There seems ot be 2 kinds of scope:
 
-Inside a function:  Local Scope (or function scope) is defined as the parameters given to a function; "var" statements within the function.
+        - Global Scope:  can be accessed from anywhere.
+        
+        - Local Scope: scope that exists within a function.
+
+
+Scope is the set of variables you can access at any given time.
+
+Inside a function:  Local Scope (or function scope) is defined as the parameters given to a function; 
+"var" statements within the function.
 
 Global Scope is the highest order scope.  Any variable defined here is available to any piece of code.  
 These variables take on more memory because the engine itself needs to hold on to this information all the time.
