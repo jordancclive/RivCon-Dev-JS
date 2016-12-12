3b8c04a54d901721316a003a00e1c0fd7e511d7c
Vince R <rivermanvr@users.noreply.github.com>
2016-12-11 23:36:16 -0500

    Update Functions-Closures.js

--- Functions-Closures.js
+++ Functions-Closures.js
@@ -5,6 +5,88 @@
 
 -----------------------------------------------------------------------------------
 
+Closures: Definition:
+
+		Closures are functions that refer to independent (free) variables 
+		i.e.: (variables that are used locally, but defined in an enclosed scope).
+		
+		In other words, these functions "remember" the environment in which they 
+		were created.
+
+	Example:
+*/	
+		function foo(){
+		  var a="a";
+		  function taz(){
+		    var z = "z";
+		    console.log(a);	//<--logs: 'a' (scope looks upward, eventually p to the global scope.)
+		  }			// When Taz accesses the variable a, it represents closure.
+		  taz()			// because the system needed to find it.
+		}
+
+		foo();
+
+
+//	Example:
+
+		//Closure topic & an example of a high order function.
+
+		var incrementCreator = function(){
+		  var numTimesCalled = 0;
+
+		  //Nested function
+		  var incFunction = function(){
+		    numTimesCalled++;
+		    console.log(numTimesCalled);
+		  }
+		  //The nested function is being 
+		  //returned out of the function
+		  return incFunction;
+		}
+
+		var incFunc = incrementCreator();
+		incFunc.toString();
+
+		/*=>(The function is returned)
+		'function incFunction() {\n    
+		numTimesCalled++;\n    
+		console.log(numTimesCalled);\n  }'
+		*/
+
+		incFunc();    //returns: 1
+
+		/* even though numTimesCalled is 
+		not declared in the incFunc function
+		& it is not part of incrementCreator
+		anymore, the function call still remembers
+		the environment it originated from.
+		( the scope it had --> all the variables it had previously)
+		and returns a '1' to track that it was called.
+
+		This is an example of Closure
+
+		This scope is stored in something called "The heap".
+
+		dir(incFunc)
+
+		function incFunction()
+
+		arguments: null
+		caller: null
+		length: 0
+		name: "incFunction"
+
+		prototype: Object
+		__proto__: function ()
+		[[FunctionLocation]]: VM985:7
+		[[Scopes]]: Scopes[2]
+
+		   0: Closure (incrementCreator) <--tracking the scope available when running.
+			 numTimesCalled: 1  <--tracking that it was called
+		   1: Global
+
+-----------------------------------------------------------------------------------
+
 Functions.
 
 Functions that return other functions are called "closures".  
