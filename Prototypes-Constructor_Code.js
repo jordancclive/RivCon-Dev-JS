120ba35d0e1d24af8222df576093f4c78af58b66
Vince R <rivermanvr@users.noreply.github.com>
2016-12-18 17:15:40 -0500

    Update Prototypes-Constructor_Code.js

--- Prototypes-Constructor_Code.js
+++ Prototypes-Constructor_Code.js
@@ -35,3 +35,39 @@ Cat.prototype.haveBaby = function (color){
     return temp;
 }
 
+/*
+---------------------------------------------------------------------
+
+                Computer simple game - Pokemon
+---------------------------------------------------------------------                
+ */           
+            
+function Pokemon(name, health, attackBonus){
+    this.name=name;
+    this.health=health;
+    this.attackBonus=attackBonus;
+}
+
+//Fixed after comments from mentor.  
+//      Methods should be separate from the constructor function.
+//      isDefeated return changed as illustrated.
+
+Pokemon.prototype.biteAttack = function(){
+    return this.attackBonus +2;
+}
+Pokemon.prototype.isDefeated = function(){
+    return this.health <=0;
+}
+
+function simulateBattle(pokemon1, pokemon2, firstMoveName){
+    do {
+        if (firstMoveName === pokemon1.name) pokemon2.health -= pokemon1.biteAttack(); 
+        else pokemon1.health -= pokemon2.biteAttack();
+        firstMoveName = (firstMoveName === pokemon1.name) ? pokemon2.name : pokemon1.name; 
+    } while((pokemon1.isDefeated() === pokemon2.isDefeated());
+    
+    if (pokemon1.isDefeated()) return pokemon2.name + " Wins!";
+    else return pokemon1.name + " Wins!";
+}
+            
+
