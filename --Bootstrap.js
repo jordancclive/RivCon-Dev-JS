c75ce9f7c7d01672c5e1afe5067746a6a93f43ac
Vince R <rivermanvr@users.noreply.github.com>
2016-12-22 22:05:12 -0500

    Update --Bootstrap.js

--- --Bootstrap.js
+++ --Bootstrap.js
@@ -6,8 +6,9 @@
 -----------------------------------------------------------------------------------
 
 All materials are in the following repository:
-*/
+
 
 https://github.com/rivermanvr/Bootstrap-Training_Working-Files
 
 -----------------------------------------------------------------------------------
+*/
