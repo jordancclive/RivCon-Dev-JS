2a1192cdd4452c568b421e32b18deb8d08832a17
Vince R <rivermanvr@users.noreply.github.com>
2016-12-30 13:30:48 -0500

    Update and rename --HTML-Backgrounds.js to --CSS-Backgrounds.js

---
+++ --CSS-Backgrounds.js
@@ -0,0 +1,37 @@
+/*
+------------------------------------------------------------------------------------------
+
+                                  Backgrounds
+
+------------------------------------------------------------------------------------------
+
+Background values you can set.
+
+        background-color            specifies the background color to be used.
+
+        background-image            specifies one or more background images to be used.
+
+        background-position         specifies the position of the bakground images.
+
+        background-size             specifies the size of the background image.
+
+        background-repeat           specifies how to repeat the background images.
+
+        background-origin           specifies the positioning area of the background images.
+
+        background-clip             specifies the painting area of the background images.
+
+        background-attachment       specifies whether background images are fixed or scrolls
+                                    with the rest of the page.
+
+        initial                     sets the property to its default value. 
+        
+        inherit                     inherits this property from its parent element.
+
+
+
+
+
+
+
+*/
