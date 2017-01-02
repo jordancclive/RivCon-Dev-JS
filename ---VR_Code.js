68396392d31e5a217b10a02aeef2339c460d2bcc
Vince R <rivermanvr@users.noreply.github.com>
2017-01-02 14:48:27 -0500

    Update ---VR_Code.js

--- ---VR_Code.js
+++ ---VR_Code.js
@@ -56,6 +56,44 @@ function flatten(arr){
     return resultArr;
 }
 
+/*---------------------------------------------
+
+		...with both forEach & reduce: 
+		
+-----------------------------------------------
+*/
+function search(func){
+	var bool = false;
+	this.forEach(function(val){
+		if(func(val)){
+			bool = true;
+		} else if(Array.isArray(val) && !bool){			
+			bool = search.call(val,func);			
+		}
+	});
+	return bool;	
+}
+
+
+
+//using reduce:
+
+function search(func) {
+	return this.reduce(function(prev, cur) {
+			debugger;
+
+		if(func(cur)) {
+			return true;
+		} else if(Array.isArray(cur)) {
+
+			if(!prev) {
+				return search.call(cur, func);
+			}
+		} 
+		return prev;
+	}, false);
+}
+
 /*
 -----------------------------------------------------------------------------
 
