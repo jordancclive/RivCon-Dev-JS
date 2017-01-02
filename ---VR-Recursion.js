1953255723028fcac66ad57a6664c3da9965a056
Vince R <rivermanvr@users.noreply.github.com>
2017-01-02 14:53:55 -0500

    Update and rename ---VR_Code.js to ---VR-Recursion.js

--- ---VR-Recursion.js
+++ ---VR-Recursion.js
@@ -1,25 +1,6 @@
 /*
 -----------------------------------------------------------------------------
 
-	Passed an object to an array
-	returned a function that:
-	
-		adds key value pairs to that object;
-		changes passed values;
-		or returns values;
-
------------------------------------------------------------------------------
-*/
-
-function accessor (obj){
-    return function(key, value){
-        if (value) obj[key] = value; 
-        return obj[key];}
-}
-
-/*
------------------------------------------------------------------------------
-
                 Recursion with flattening 
                 a multi-dimensional array
 
