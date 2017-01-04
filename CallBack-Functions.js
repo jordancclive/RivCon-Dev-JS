584728d5f318efd1496e5f78546fbc7ba7db4e1a
Vince R <rivermanvr@users.noreply.github.com>
2017-01-03 21:57:30 -0500

    Update CallBack-Functions.js

--- CallBack-Functions.js
+++ CallBack-Functions.js
@@ -119,7 +119,37 @@ An example:
               2
               3
               4
+	      	      
+    ----------------------------------------------- 	      
+*/	      
+function filter(arr, func){
+	var finalArr = [];
 
+	arr.forEach(function(index){
+		if(func(index)){
+			finalArr.push(index);
+		}
+	});
+
+	return finalArr;
+}
+
+/*
+---------------------------------------more:
+*/
+
+function search(func){
+	var bool = false;
+	this.forEach(function(val){
+		if(func(val)){
+			bool = true;
+		} else if(Array.isArray(val) && !bool){			
+			bool = search.call(val,func);			
+		}
+	});
+	return bool;	
+}	      
+/*
 ----------------------------------------------------------------------------------------
 
 			.filter(function(value, index));
@@ -170,7 +200,52 @@ An example:
           	
           }
           
-          console.log(shortNamesOnly(people));         
+          console.log(shortNamesOnly(people));       
+
+    //-----------------------------------------------
+
+/*
+-----------------------------------------------------------------------------
+
+		Using filter to find the differences
+		between 2 arrays
+		
+		then:
+		tying it together with finding
+		the Symmetric Difference between the arrays.
+		
+
+-----------------------------------------------------------------------------
+*/
+// iterative version
+// function difference(arr1, arr2){
+//     let resultArr = [];
+//     if (arr1.length === 0) return [];
+//     for (let i=0;i<arr1.length; i++){
+//         if(arr2.indexOf(arr1[i]) <0) resultArr.push(arr1[i]);
+//     }
+//     return resultArr;
+// }
+
+// using filter
+function difference(arr1, arr2){
+    if (arr1.length === 0){
+        return [];
+    } 
+    return arr1.filter(function(elem){
+        if(arr2.indexOf(elem)<0)return elem;
+    });
+}
+
+function symmetricDiff(arr1, arr2){
+    if (arr1.length === 0){
+        return [];
+    } 
+    let resultArr=[];
+    return difference(arr1, arr2).concat(difference(arr2, arr1));
+}
+
+
 /*
 ----------------------------------------------------------------------------------------
 
@@ -287,7 +362,15 @@ An example:
     result:
 	newArr:  [ 2, 3, 4, 5 ]		The return value within .map passes a new array result.
 	arr:  [ 1, 2, 3, 4 ]		The original array is not changed.
-
+	
+    //-----------------------------------------------
+*/    
+function callAll(obj, arrOfFunc){
+	return arrOfFunc.map(function(elem){
+		return elem.call(obj);
+	});
+}  
+/*
 ----------------------------------------------------------------------------------------
 
 			.reduce(function(value, index)); 
@@ -421,6 +504,43 @@ Example - Using .reduce to make a filter:
     }
 
     countRepeats(arr);		//returns: { corey: 1, mom: 1, dad: 1, doug: 3 }
+
+    //-----------------------------------------------
+
+function reject(inputArr, funcCall){
+    return inputArr.reduce(function(filtArr, elem){
+        if (!funcCall(elem)){
+               filtArr.push(elem);
+        }
+        return filtArr;
+    },[]);   
+}
+
+//--------------------------
+
+var callReduceOnThisArray = [1,2,3,4,5];
+
+var sum = callReduceOnThisArray.reduce(function(total,elem){
+      return total + elem;
+    },100);
+
+//---------------------------
+
+function search(func) {
+	return this.reduce(function(prev, cur) {
+			debugger;
+
+		if(func(cur)) {
+			return true;
+		} else if(Array.isArray(cur)) {
+
+			if(!prev) {
+				return search.call(cur, func);
+			}
+		} 
+		return prev;
+	}, false);
+ }
 
 /* 
 ----------------------------------------------------------------------------------------
