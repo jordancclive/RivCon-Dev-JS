341b10890e7c440200436725b541819dd9f100a3
Vince R <rivermanvr@users.noreply.github.com>
2017-01-05 13:39:11 -0500

    Update Algorithms-Shuffling.js

--- Algorithms-Shuffling.js
+++ Algorithms-Shuffling.js
@@ -5,32 +5,22 @@
 
 ----------------------------------------------------------------------------
 
-See:  https://bost.ocks.org/mike/shuffle/compare.html
+See:  https://bost.ocks.org/mike/shuffle/
+
+      https://bost.ocks.org/mike/shuffle/compare.html
+
+      
 
 ----------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 */
+function shuffle(array) {
+  var m = array.length, t, i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+}
+
+//randomizes the original array in-place.
