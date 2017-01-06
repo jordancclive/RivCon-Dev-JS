ba004945c8321b4da15ff5a3db6a0b1262e01738
Vince R <rivermanvr@users.noreply.github.com>
2017-01-05 22:54:32 -0500

    Update Recursion-Tricky_Code.js

--- Recursion-Tricky_Code.js
+++ Recursion-Tricky_Code.js
@@ -1,6 +1,118 @@
 /*
 -----------------------------------------------------------------------------
 
+		Weaving multiple arrays into one array
+
+-----------------------------------------------------------------------------
+*/
+
+function interleave(){
+ 
+    //coming in recursively, there will be only one argument at index: [0]
+    //and it will be an array.  Need to compensate for this.
+
+    let args;
+    if (Array.isArray(arguments[0]) && arguments.length ===1){
+        args = arguments[0];
+    }else{
+        args = Array.prototype.slice.call(arguments); 
+    }  
+    //args is now a usable array.
+    
+    let strToMix = args.length;     //shows how many args-strings exist.
+    let index = 0;                  //largest index for all args
+    let workingStr = '';
+
+    //base case - Mix the 0 index together on all the possible strings
+    for(let i=0; i<strToMix; i++){
+          if (args[i][0]){                
+              workingStr += args[i][0];
+              args[i] = args[i].slice(1);
+              if (index < args[i].length) index = args[i].length;
+          }
+    }   
+    //recursive case
+    if (index>0){
+        workingStr += interleave(args); 
+    } 
+    return workingStr;   
+}
+
+/*
+Wow, never considered using recursion for this. 
+Great job figuring out how!
+*/
+
+/*  This (below) is the code I did in repl. 
+    It has all of the console.logs in it and I want
+    to keep them as a reminder of how I did this function:
+*/
+
+function interleaveVR(){
+    /*
+        ---------------------------------------------------------------------
+        ******did this in repl ******
+        ---------------------------------------------------------------------
+
+    we need to use the arguments object.
+    2 strings of equal length: Mix- add in order.
+    2 strings unequal length: Mix - extra characters on the end.
+    3-n number of equal length strings: Mix - add in order.
+    */
+    
+    //coming in recursively, there will be only one argument at index: [0]
+    //and it will be an array.  Need to compensate for this.
+    let args;
+    if (Array.isArray(arguments[0]) && arguments.length ===1){
+        args = arguments[0];
+    }else{
+        args = Array.prototype.slice.call(arguments); 
+    }  
+    //args is now a usable array.
+    
+    let strToMix = args.length;     //shows how many args-strings exist.
+    let index = 0;                  //largest index for all args
+    let workingStr = '';
+    
+    console.log('args array: ',args);
+    console.log('index at start of function: ', index, '\n'); 
+
+    //base case - Mix the 0 index together on all the possible strings
+    for(let i=0; i<strToMix; i++){
+          if (args[i][0]){
+            
+              console.log('         args[i] in the loop, before slice: ', args[i], '\n');                
+              
+              workingStr += args[i][0];
+              args[i] = args[i].slice(1);
+              if (index < args[i].length) index = args[i].length;
+              
+              console.log('         index in base case loop: ', index);                  
+              console.log('         args[i] after slice: ', args[i]);
+              
+          }
+          
+          console.log('\n','  workingStr after if, still in loop: ', workingStr,'\n');
+    }
+
+    console.log(' index after base case: ', index,'\n');
+    console.log(' args array: ',args);
+    console.log('\n','----------------------------------------');  
+    console.log(' ----------recursive case coming---------');
+    console.log(' ----------------------------------------','\n');
+    
+    
+    //recursive case
+    if (index>0){
+        workingStr += interleave(args); 
+    } 
+    return workingStr;   
+}
+
+
+/*
+-----------------------------------------------------------------------------
+
                 Recursion with flattening 
                 a multi-dimensional array
 
