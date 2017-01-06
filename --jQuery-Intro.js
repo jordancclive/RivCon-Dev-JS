68975d1b8e0b37bd9d0fa93313eb07b004f16763
Vince R <rivermanvr@users.noreply.github.com>
2017-01-06 18:08:08 -0500

    Update --jQuery-Intro.js

--- --jQuery-Intro.js
+++ --jQuery-Intro.js
@@ -68,11 +68,66 @@ The Document Object Model (DOM)
                                                               JS that was sent to interact with the DOM.
     -------------------                                      --------------------------------------------
     
+    
+   **** important ***
+   
+   Different Browsers have a slightly different DOM, and jQuery can comunicate to all of them easily.
+   
+   with this stmt--->  jQuery(document);
+   
+   to find different elements:    jQuery("h1"); or in shorthand:  $("h1");
+                                  jQuery("p");                    $("p");
+    
 --------------------------------------------------------------------------------
                                 
+Task: find and change an element on a page: use the .text() method.
+
+        $("h1").text();   --->returns:  "Where do you want to go?"
+        
+        $("h1").text("Where to?");   --->replaces the text to:  "Where to?"
+
+--------------------------------------------------------------------------------        
+
+*** important ***
+
+The DOM takes a bit of time to build the tree structure 
+and JS might execute before the DOM loads.  If it does, 
+and your JS interacts with the DOM you will get an error. 
+Note:  When the DOM is ready, it triggers an event...you need to listen for it.
+
+So you need to do this:
+
+            jQuery(document).ready(function(){
+                <code>
+            });
+
+--------------------------------------------------------------------------------
+
+                            Downloading jQuery
+
+--------------------------------------------------------------------------------
+
+Go to www.jQuery.com
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+--------------------------------------------------------------------------------
+
+                              Traversing
+
+--------------------------------------------------------------------------------
 
 
 
