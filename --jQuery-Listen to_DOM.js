7ee2f8602804632ff0f43cc016eb6859e2fa1499
Vince R <rivermanvr@users.noreply.github.com>
2017-01-08 21:23:00 -0500

    Update --jQuery-Listen to_DOM.js

--- --jQuery-Listen to_DOM.js
+++ --jQuery-Listen to_DOM.js
@@ -87,7 +87,7 @@ Our event handler:
                               Listening to
                                DOM events
                                
-                             Keyboard events
+                              Mouse events
                               
 --------------------------------------------------------------------------------
 
@@ -122,19 +122,80 @@ Clean up the code:
             $('.confirmation').on('mouseenter', 'h3', showTicket);             
      }));
 
+--------------------------------------------------------------------------------
+
+                              Listening to
+                               DOM events
+                               
+                             Keyboard events
+                              
+--------------------------------------------------------------------------------
+
+Some keyboard methods to know:
+
+        Keyboard Events           Form Events
+
+        keypress                  blur      select
+        keydown                   change    focus
+        keyup                     submit
+
+        http://api.jquery.com/category/events/keyboard-events/
+        http://api.jquery.com/category/events/form-events/
+
+--------------------------------------------------------------------------------
+
+Helper methods for getting input:
+
+        .val()              <--- gets the value
+        .val(new value)     <--- sets the value.
+
+--------------------------------------------------------------------------------
+
+DOM
+
+document
+    li    .vacation
+        p
+            input   .quantity     <---a person will type a quantity with the keyboard
+    p                                 (an input field)
+        span    #total            <-- this is what we are going to change.
+
+--------------------------------------------------------------------------------
+
+Code:
+
+        $(document).ready(function(){
+            $('.vacation').on('keyup', '.quantity', function(){
+                //get the price for this vacation
+                //get the qty entered
+                //set the total to price* qty
+            });
+        });
+        
+        Will be inserting:
+        
+                var price = +$(this).closest('.vacation').data('price');   <--- gets the price. returns a string
+                                                                                the plus turns it into a number.
+                var quantity = +$(this).value();      <--- returns The value inputed from the keyboard.  Which
+                                                          is the thing that fired the event handler, hence "this".
+                $('#total').text(price*quantity);     <---Puts the value on the screen.
+
 -----------------------------------
 
+The resulting code:
 
+        $(document).ready(function(){
+            $('.vacation').on('keyup', '.quantity', function(){
+                var price = +$(this).closest('.vacation').data('price');  
+                var quantity = +$(this).value();    
+                $('#total').text(price*quantity);   
+            });
+        });
 
-
-
-
-
-
-
-
-
-
-
-
+--------------------------------------------------------------------------------
 */
+
+
+
+
+
