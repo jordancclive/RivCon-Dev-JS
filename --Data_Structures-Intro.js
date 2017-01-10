cf84b95ec7fd2315da4c6d4d0654b93be04d9bf6
Vince R <rivermanvr@users.noreply.github.com>
2017-01-10 13:11:59 -0500

    Update --Data_Structures-Intro.js

--- --Data_Structures-Intro.js
+++ --Data_Structures-Intro.js
@@ -6,14 +6,58 @@
 
 --------------------------------------------------------------------------------
 
+Abstract data types and data structures are fundamental to general computer science, 
+and are also a great way to better understand object-oriented programming (OOP).
+
+--------------------------------------------------------------------------------
+
+Abstract Data Type (ADT)
+
+        An ADT is a description of information, how that information is connected, 
+        and performable operations on the information. 
+        
+        For example:
+        
+                a list is an ordered collection of elements, which you can add to or read from.
+                
+                A dictionary is a set of key-value pairs, where you can get or set a value 
+                (e.g. "Emily") by key (e.g. "name"). 
+                
+        Notice that an ADT says nothing about programs, memory, steps, etc. — it's just a concept.
 
 
+Data Structure (DS)
 
+        A DS on the other hand is a specific programmatic solution for storing, referencing, 
+        and accessing data in computer memory. 
+        
+        The purpose of a data structure is to implement some ADT — 
+        
+        for example: 
+        
+                take the concept of a "list" and realize it in actual code 
+                (place such-and-such data at this memory address, increment a variable, etc.).
 
+        Often an ADT can be implemented via more than one DS. 
+        Since different DSs have advantages and disadvantages from functionality 
+        and performance standpoints, it is beneficial to have a solid understanding of how they work.
 
+--------------------------------------------------------------------------------
 
+Also, the workshop includes references to the following Abstract Data Types (ADT) and Data Structures (DS), 
+which you may optionally elect to research on your own time:
 
+Queue ADT
+Stack ADT
+Array DS
+Circular Buffer DS
+Linked List DS
+Association List DS
+Associative Array / Map / Dictionary ADT
+Hash Table DS
+Binary Search Tree DS
 
+--------------------------------------------------------------------------------
 
 
 
