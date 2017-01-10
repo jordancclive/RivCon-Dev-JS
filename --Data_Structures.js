1fa409a640162fb7694e653e9c1b890c20c22572
Vince R <rivermanvr@users.noreply.github.com>
2017-01-10 14:01:38 -0500

    Update and rename --Data_Struc-Stacks&Queues.js to --Data_Structures.js

---
+++ --Data_Structures.js
@@ -0,0 +1,97 @@
+/*
+---------------------------------------------------------------------------------
+
+                                Stacks & Queues
+
+---------------------------------------------------------------------------------
+
+Stacks:
+
+        Like a stack of books stacked on a desk.  You stack them from bottom to top
+        and you remove them from top to bottom.
+        
+              - Elements are sorted in insertion order
+              - Last in, first out
+              - Elements have no index
+              - Can only add to the top and remove from the top.  (push in & pop out)             
+              
+              - Useful when you want to reverse the order of elements.  (undo on your computer)
+              
+Queues
+
+        A single file line.
+        
+              - Have a front and a back.
+              - Can add to the back and remove from the front.  
+              - adding: enqueueing and removing: ddequeueing
+              
+              - elements are sorted by insertion order
+              - FIFO
+              - Elements have no index
+              - Can only add to back and remove from front.
+              
+              - A print queue. 
+              
+ ---------------------------------------------------------------------------------
+
+                               Linked Lists
+                               
+---------------------------------------------------------------------------------    
+        
+A List:
+
+        Dictionary: A number of connected items or names written or printed consecutively.
+
+        In Computer Science:  
+                
+                an ordered collection of values (aka: items, entries, elements)
+                
+                Where a value may occur more than once.
+                
+A linked List:  
+
+        A sequence of nodes where one is a reference and the other is a reference to the next item on the list.
+        
+        The list ends with a reference pointer of null.
+        
+        Usually there is an extra reference that points to the beginning called 'head'
+              
+Behavior:
+
+        You can add and remove an element.
+        
+        find an element and do operations on elements
+        
+        
+How are they implemented:
+
+        - Arrays or Linked Lists.
+        
+        The problem with arrays is when you have to add an element in the middle of the list.  
+        You have to shift the elements down to make room.
+        
+        An array's size is fixed after you create it.
+        
+ ---------------------------------------------------------------------------------
+
+                               Binary Search Tree
+                               
+---------------------------------------------------------------------------------  -    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
