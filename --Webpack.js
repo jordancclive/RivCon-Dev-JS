37aca1e4b1dc4f9a197e63ac3c8e46d163fa17b3
Vince R <rivermanvr@users.noreply.github.com>
2017-01-14 16:27:37 -0500

    Update --Webpack.js

--- --Webpack.js
+++ --Webpack.js
@@ -117,13 +117,15 @@ Now we are ready for Webpack
                                       
               There is a 'node_modules' folder' in our directory (which is where Webpack is saved to by default).
               Also got my package.json file and our .git file.
+              
+        --------------------------------------------------              
  
- 
-              type:     Mac-Pro-370:WebpackDir designadmin$ ./node_modules/webpack/bin/webpack.js
+              type:     Mac-Pro-370:WebpackDir designadmin$ ./node_modules/webpack/bin/webpack.js   <---runs webpack
 
               result:   a bunch of stuff with the end saying:  Output filename not configured.
                         ...so I ran the webpack executable and got an error message.
-          
+
+
               Discussion:
                         I need to create a config.js file for webpack in order for it to run.
                         let's do that & open our code editor:
@@ -131,7 +133,91 @@ Now we are ready for Webpack
               Type: touch webpack.config.js
                     code .
                     
-              
+              Discussion:
+                        I can now see my empty 'webpack.config.js' file.  
+                        At a basic level I need to specify 'module.exports = {};
+                        and I need to specify an entry point and an output within the object.
+                        
+                        my entry is going to be:  entry: './src/index',     <---which doesn't exist yet
+                        my output --->  output: {
+                                            path: './dist',
+                                            filename: 'bundle.js'
+                                        }
+                                        
+                        The final statement:
+                        
+                                  module.exports = {
+                                      entry: './src/index',
+                                      output: {
+                                            path: './dist',
+                                            filename: 'bundle.js'
+                                      }
+                                  };
+    
+    
+                        So, if I save this and run things again, it runs, but I get a different error:
+                        
+                        result:
+                        
+                              Mac-Pro-370:WebpackDir designadmin$ ./node_modules/webpack/bin/webpack.js
+                              Hash: 9806551c60c2f4802965
+                              Version: webpack 1.14.0
+                              Time: 30ms
+
+                              ERROR in Entry module not found: Error: Cannot resolve 'file' or 'directory' 
+                              ./src/index in /Users/designadmin/Documents/gitRepositories/FlexClass/WebpackDir
+                              design-admins-Mac-Pro-370:WebpackDir designadmin$
+                                                
+                    
+              Discussion:
+                        It, of course, finds the config file, but cannot find the 'index' file
+                        nor the 'src' directory.  
+                        
+                        So let's make them now in the editor:
+                        
+                            - mkdir src (within our WebpackDir)
+                            - touch index.js (in our src directory)
+                            - inside index.js, let's type:  console.log('hello');
+                            
+                        Now, let's run ./node_modules/webpack/bin/webpack.js again in the console.
+                        
+                        result:                        
+                        
+                              Hash: 2f0583dc637cc12cae9a
+                              Version: webpack 1.14.0
+                              Time: 56ms
+                               Asset     Size  Chunks             Chunk Names
+                              bundle  1.41 kB       0  [emitted]  main
+                                 [0] ./src/index.js 21 bytes {0} [built]
+
+                        *** Everything runs properly. ***
+                        
+              Discussion:
+                        Now that the command ran, let's take another look at our WebpackDir directory:
+                        
+                        We will see:
+                        
+                              The command created our 'dist' folder'
+                              the file 'bundle.js' now also exists.
+                              
+                              If you look at bundle.js  , you will see a bunch of stuff,
+                              but at the bottom you will see our console.log statement:
+                              
+                                        /******/ ([
+                                        /* 0 */
+                                        /***/ function(module, exports) {
+
+                                          console.log('hello');
+
+                                        /***/ }
+                                        /******/ ]);                             
+                              
+                              
+                        
+                        
+                        
+                        
+                                      
               
                                       
                                    
