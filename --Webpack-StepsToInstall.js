35213e3cb4f0181b373de2449e248067d162a71d
Vince R <rivermanvr@users.noreply.github.com>
2017-01-16 16:35:58 +0000

    Update --Webpack-StepsToInstall.js

--- --Webpack-StepsToInstall.js
+++ --Webpack-StepsToInstall.js
@@ -6,32 +6,55 @@
 
 ----------------------------------------------------------------------------------
 
+01-  create your high level project directory for
+02-  type: npm init -y              <--setting up a default environment
+                                       creates a new 'package.jspn' file
+03-  git init                       <--setup git for this project
+                                      Note: ***** still need to figure out how git will exclude node_modules *****
+04-  npm install webpack --save
+05-  touch webpack.config.js        <--create config.js file for webpack in primary project dir
+06-  config contents:
 
+        module.exports = {
+            entry: './src/index',           //<--src = directory for your .js project files
+            output: {                       //   index will be your top/first/entry .js in the bundle
+                path: './dist',
+                filename: 'bundle.js'       //<--always output to dist/bundle.js 
+            },
+            module: {
+                loaders: [
+                    {
+                        test: /\.js$/,      //<--operate on files that end in .js
+                        loader: 'babel',    //<--The loader is babel
+                        query: {            //<--we are going to query the es2015 preset
+                            presets:    ['es2015']
+                        }
+                    }
+                ]
+            }
+        };
+07-  change package json: add needed scripts:
 
+         "scripts": {
+            "test": "echo \"Error: no test specified\" && exit 1",
+            "webpack":  "./node_modules/webpack/bin/webpack.js -w"
+          },
+          
+          Note: now you can use npm run webpack in the terminal console.
+          
+08-   create index.js (any name.js)
 
+          Note: other .js files will be identified here with an import cmd
+                     ex: import Foo from './Foo';
+                These other files will have a corresponding export
+                     ex: export default Foo;
+                     
+          Note: ***** in Foo.js, is the export refering to the class or the filename?*****
+                      ....I am assuming the class.
+                      
+09. npm install babel-loader --save
+10. npm install babel-core --save
+11. npm install babel-preset-es2015 --save
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+***The directory is ready for development***
 */
