88b335ce87f2f744023b793ca98afc3773c77c5a
Vince R <rivermanvr@users.noreply.github.com>
2017-01-16 19:16:23 -0500

    Create --Git-Collaborator_S-U.js

---
+++ --Git-Collaborator_S-U.js
@@ -0,0 +1,45 @@
+/*
+--------------------------------------------------------------------------
+
+                              Git
+
+                   s/u a collaberator on your repor
+
+--------------------------------------------------------------------------
+
+steps:
+
+go to collaberators
+
+get user name
+
+add that person
+
+slack the address to that person
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
