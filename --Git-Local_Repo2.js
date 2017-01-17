a8a826f45fbef51e856fca36cf1c97b56e3c41f3
Vince R <rivermanvr@users.noreply.github.com>
2017-01-16 19:13:37 -0500

    Update and rename --Git.js to --Git-Local_Repo2.js

---
+++ --Git-Local_Repo2.js
@@ -0,0 +1,56 @@
+/*
+-----------------------------------------------------------------------------
+
+                          Git repository setup (detour)
+                           When a forking error occurs
+-----------------------------------------------------------------------------
+
+Steps:
+
+Clone the external repository to your local machine
+
+ls -a
+
+rm -rf .git     <-- removed git - forced a separation from where you were cloned from.
+
+Add your own repo on git.com    -----choice:  you want to push from cmd line.
+
+git init the local directory
+
+git add .
+git commit...
+
+run the 2 commands you got from the git.com site for local upload.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
