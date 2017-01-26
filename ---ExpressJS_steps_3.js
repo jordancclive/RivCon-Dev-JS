f7f3f895365107ccd8370058602881b3806a741a
Vince R <rivermanvr@users.noreply.github.com>
2017-01-26 11:11:01 -0500

    Create ---ExpressJS_steps_3.js

---
+++ ---ExpressJS_steps_3.js
@@ -0,0 +1,15 @@
+/*
+---------------------------------------------------------------------------------------
+
+                                      Express.js
+                                       Step #3
+                                       
+                                      Lodash...
+                                      
+---------------------------------------------------------------------------------------
+
+A utilities library: Lodash. Lodash provides a long list of useful JavaScript functions. 
+*/
+
+        // in the tweetBank.js module install lodash
+        npm install lodash --save
