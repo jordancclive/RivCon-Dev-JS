6df89a6edb410f57e798662a87232f9de1332012
Vince R <rivermanvr@users.noreply.github.com>
2017-01-26 00:45:06 -0500

    Update ---ExpressJS_steps_2.js

--- ---ExpressJS_steps_2.js
+++ ---ExpressJS_steps_2.js
@@ -1,1 +1,39 @@
+/*
+---------------------------------------------------------------------------------------
+
+                                      Express.js
+                                       Step #2
+                                       
+                                      Templates
+                                      
+---------------------------------------------------------------------------------------
+
+A templating engine is a program that renders final documents (e.g. HTML files) based on templates. 
+A template is an outline or plan for a document, which can be combined with data to complete it.
+
+            template file + locals object -> rendering function -> complete html
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
