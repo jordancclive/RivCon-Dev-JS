33551cd8feea1f6b6c1407eec2f016c35f5fa23d
Vince R <rivermanvr@users.noreply.github.com>
2017-01-26 00:39:36 -0500

    Update and rename ---ExpressJS_steps.js to ---ExpressJS_steps_1.js

--- ---ExpressJS_steps_1.js
+++ ---ExpressJS_steps_1.js
@@ -35,6 +35,13 @@
         const express = require( 'express' );   //store the export within express in a variable
         const app = express();                  //create an instance of an express aplication
 
+        /*  Note:  What follows below this line is the repo developing into a fully functional application
+            Once this is done and you upload it to some server, the user must tye npm install in order
+            to install all the dependencies in the package.json file in order to run the application
+            on their machine.     */
+
+        npm install   
+
 
 //-------------------------------------------------------
         const express = require ( 'express' );
@@ -57,8 +64,66 @@
 //                    Middleware
 //-------------------------------------------------------
 
+        const express = require ( 'express' );
+        const chalk = require ( 'chalk' );
+        const app = express();
 
+        app.use('/special/', function (req, res, next) {        // <---Question for class, redirected, 
+                if (req.url.length >1){                         //     but what if a file name was here
+                    res.redirect('/special');
+                }
+                next();
+            })
 
+        app.use(function (req, res, next) {
+            console.log('method: ', req.method, ' url: ', req.url);
+            res.on('finish', function(){
+                console.log('Response code: ', res.statusCode);
+            })
+            next();
+        })
+
+        app.get('/', function (req, res, next) {
+          res.send('Hello World!')
+        })
+
+        app.get('/news', function (req, res, next) {
+          res.send('You visited the /news directory')
+        })
+
+        app.get('/special/', function (req, res, next) {
+          res.send('You visited the /special directory')
+        })
+
+        app.listen(3000, function(){
+            console.log('listening to port 3000');
+        });
+
+//-------------------------------------------------------
+
+//package.json file:
+
+        {
+          "name": "twitter-js",
+          "version": "1.0.0",
+          "description": "Bldg a simple Twitter clone to learn express.js",
+          "main": "./routes/index.js",
+          "scripts": {
+            "start": "nodemon app.js"
+          },
+          "repository": {
+            "type": "git",
+            "url": "twitter-js"
+          },
+          "author": "Vincent Rios",
+          "license": "ISC",
+          "dependencies": {
+            "chalk": "^1.1.3",
+            "express": "^4.14.0"
+          }
+        }
+
+//-------------------------------------------------------
 
 
 
