427d0620baa40984a9b389666963c265ed11e217
Vince R <rivermanvr@users.noreply.github.com>
2017-01-26 13:06:39 -0500

    Create --HTTP-Intro.js

---
+++ --HTTP-Intro.js
@@ -0,0 +1,15 @@
+/*
+---------------------------------------------------------------------------------------
+
+                                      HTTP
+                                      
+---------------------------------------------------------------------------------------
+
+HTTP is the communications standard driving the web. 
+It uses the client-server model, where the server has resources which clients might want. 
+
+A client can request a resource, in which case the server can respond with data. 
+
+The server cannot initiate communication; it can only responds to requests.
+
+*/
