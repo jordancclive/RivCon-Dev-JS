e1eee90f375402e9eba8d8cc9a7c753605f1a0bb
Vince R <rivermanvr@users.noreply.github.com>
2017-01-30 10:59:20 -0500

    Create --Herokuapp.js

---
+++ --Herokuapp.js
@@ -0,0 +1,30 @@
+/*
+---------------------------------------------------------------------
+
+                            Herokuapp
+
+---------------------------------------------------------------------
+
+1. get to your directory with the code.
+2. Be sure your package.json has the right information
+
+    heroku login
+    <your credentials>
+    heroku create
+    heroku push
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
