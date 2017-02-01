16b89aaf93757b33559e859e6fa8e0b9683ee105
Vince R <rivermanvr@users.noreply.github.com>
2017-02-01 11:22:27 -0500

    Update ---SQL Intro.js

--- ---SQL Intro.js
+++ ---SQL Intro.js
@@ -1,1 +1,38 @@
+/*
+-----------------------------------------------------------------------------------
 
+                                        SQL Intro
+
+-----------------------------------------------------------------------------------
+
+select (column) from (table) where ..... (an expression)
+
+--> select * (gets everything)
+
+select * from movies where title = 'The Kid';   <-- if you want an exact match use single quotes.
+--------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
