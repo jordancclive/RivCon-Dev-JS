550e51963bb749615996b126e1136bab9e5bd537
Vince R <rivermanvr@users.noreply.github.com>
2017-03-08 19:55:25 -0500

    Update ---HTTP.js

--- ---HTTP.js
+++ ---HTTP.js
@@ -5,11 +5,58 @@
 
 -------------------------------------------------------------------------
 
-'/users/:name/tasks?status=active'
-  ==> req.query.status === 'active'
+        '/users/:name/tasks?status=active'
+          ==> req.query.status === 'active'
+
+        '/users/:name/tasks?language=javascript'
+          ==> req.query.language === 'javascript'
+
+        '/users/:name/tasks?fullstack=academy'
+          ==> req.query.fullstack === 'academy'
   
-'/users/:name/tasks?language=javascript'
-  ==> req.query.language === 'javascript'
   
-'/users/:name/tasks?fullstack=academy'
-  ==> req.query.fullstack === 'academy'
+  -------------------------------------
+  
+  Look at the following:
+  
+  
+          router.get('/users/:userName/tasks', function(req, res, next) {
+          // do something
+          });
+        
+          router.get('/users/:userName/tasks?status=complete', function(req, res, next) {
+          // don't make hardcoded route for query!
+          });
+  
+  Think about this:
+  
+          router.get('/users/:userName/tasks', function(req, res, next) {
+
+            // when route is '/users/billy/tasks?status=complete'
+            // req.params.userName === 'billy'
+            // req.query.status === 'complete'
+
+            // when route is '/users/billy/tasks'
+            // req.query === {}
+            // req.query.status === undefined
+          });
+
+          router.get('/users/:userName/tasks', function(req, res, next) {
+
+            // when route is '/users/billy/tasks?status=complete'
+            // req.params.userName === 'billy'
+            // req.query.status === 'complete'
+
+            // when route is '/users/billy/tasks'
+            // req.query === {}
+            // req.query.status === undefined
+          });
+
+          // if you want to check if query is given or not,
+          // you can check req.query.status === undefined
+
+          // however, if you try to check whether req.query is exist or not,
+          // such as 
+          // if (req.query) { // do something }
+
+          // req.query value is always true, because even when there's no given query, it has empty object value, which has truthy value
