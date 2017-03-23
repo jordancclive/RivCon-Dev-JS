6f71ed95c9010cb234791e63f522f9d93d86962d
Vince R <rivermanvr@users.noreply.github.com>
2017-03-22 23:20:21 -0400

    Create ---React-modules.js

---
+++ ---React-modules.js
@@ -0,0 +1,21 @@
+/*
+---------------------------------------------------------
+
+                React modules      
+
+---------------------------------------------------------
+
+To import other modules into our file, we can use require, 
+and export using module.exports!
+*/
+
+const React = require('react');
+
+class Main extends React.Component {
+  /** Hello world! **/
+}
+module.exports = Main;
+
+
+
+
