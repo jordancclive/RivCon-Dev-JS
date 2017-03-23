69a0fb8cb7e8b2f2cbab17ec3626f9f2c2d72b34
Vince R <rivermanvr@users.noreply.github.com>
2017-03-23 12:54:50 -0400

    Create ---React-Setup_Components.js

---
+++ ---React-Setup_Components.js
@@ -0,0 +1,19 @@
+/*
+-------------------------------------------------------------------------------
+
+                Setting up a component
+
+-------------------------------------------------------------------------------
+
+    Creating the component in Main.js means that 
+    you create the component/class itself in Main.js 
+    and export it, like so:
+*/
+
+        class Main extends React.Component {
+          render () {
+            /* etc…*/
+          }
+        }
+
+        export default Main
