d016997c4495971595416400386ed750c85e48b4
Vince R <rivermanvr@users.noreply.github.com>
2017-04-17 10:00:26 -0400

    Create ---Google-Developers.js

---
+++ ---Google-Developers.js
@@ -0,0 +1,24 @@
+/*
+---------------------------------------------------------------------------------
+
+            Google developers web site for getting APIs
+            
+---------------------------------------------------------------------------------
+
+---->   console.developers.google.com
+
+---------------------------------------------------------------------------------
+
+I was getting the youtube api:  
+
+      YouTube Data API v3	The YouTube Data API v3 is an API that provides access to YouTube data, 
+      such as videos, playlists, and channels.
+      
+      got an API key and also installed a model as follows:
+      
+      npm install youtube-api-search --save
+
+
+
+
+*/
